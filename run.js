68730e7f7064269d2c11191f0558be5b7c50c80b
junhao <changjunhao@outlook.com>
2016-06-22 15:01:33 +0800

    init scheduler

--- run.js
+++ run.js
@@ -1,223 +1,44 @@
-/**
- * ux crawler entrance 
- */
-////log setting////////////////////////////////////////////////////////////////////
-var logging = require('./lib/logging.js'); 
-////arguments parse///////////////////////////////////////////////////////////////
-var userArgv = require('optimist')
-.usage('Usage: $0 -i [instance name] -a [crawl|test|config|proxy|schedule]  -p [num] -l[url] -h')
-.options('i', {
-        'alias' : 'instance',
-        'default' : 'pengtouba',
-        'describe' : 'Specify a instance',
-        'demand' : true
-    })
-.options('a', {
-        'alias' : 'action',
-        'default' : 'crawl',
-        'describe' : 'Specify a action[crawl|test|config|proxy|schedule]',
-        'demand' : true
-    })
-.options('p', {
-        'alias' : 'port',
-        'default' : 2013,
-        'describe' : 'Specify a service port, for config service and proxy router'
-    })
-.options('h', {
-        'alias' : 'help',
-        'describe' : 'Help infomation'
-    });
+var EventEmitter = require( 'events' ).EventEmitter;
+var argv = require( 'minimist' )( process.argv.slice( 2 ) );
+var fs = require( 'fs' );
+var path = require( 'path' );
 
-var options = userArgv.argv;
-if(options['h']){userArgv.showHelp();process.exit();}
-var settings = require('./instance/'+options['i']+'/'+'settings.json');
-settings['instance'] = options['i'];
-////log level/////////////////////////////////////////////////////////////////
-var log_level = 'DEBUG';
-if(settings['log_level'])log_level = settings['log_level'];
-////crawling action///////////////////////////////////////////////////////////
-var crawling = function(){
-	var logger = logging.getLogger('crawling',options['i'],log_level);
-    settings['logger'] = logger;
-    settings['instance'] = options['i'];
-    var spider = new (require('./spider'))(settings);
+var logger = require( './lib/logger.js' );
 
-    spider.start();
-}
-var meipai = function(){
-    var logger = logging.getLogger('meipai',options['i'],log_level)
-    settings['logger'] = logger
-    settings['instance'] = options['i']
-    var mpSpider = new (require('./meipai'))(settings);
-    mpSpider.start();
-}
-var youku = function () {
-    var logger = logging.getLogger('youku',options['i'],log_level);
-    settings['logger'] = logger;
-    settings['instance'] = options['i'];
-    var spider = new (require('./youku'))(settings);
-    spider.start();
-}
-var miaopai = function () {
-    var logger = logging.getLogger('miaopai',options['i'],log_level);
-    settings['logger'] = logger;
-    settings['instance'] = options['i'];
-    var spider = new (require('./miaopai'))(settings);
-    spider.start()
-}
-var bili = function () {
-    var logger = logging.getLogger('bilibili',options['i'],log_level);
-    settings['logger'] = logger;
-    settings['instance'] = options['i'];
-    var spider = new (require('./bili'))(settings);
-    spider.start()
-}
-var tx = function () {
-    var logger = logging.getLogger('腾讯视频',options['i'],log_level);
-    settings['logger'] = logger;
-    settings['instance'] = options['i'];
-    var spider = new (require('./tx'))(settings);
-    spider.start()
-}
-var le = function () {
-    var logger = logging.getLogger('乐视',options['i'],log_level);
-    settings['logger'] = logger;
-    settings['instance'] = options['i'];
-    var spider = new (require('./le'))(settings);
-    spider.start()
-}
-var creatTT = function () {
-    var logger = logging.getLogger('creatTT',options['i'],log_level)
-    settings['logger'] = logger
-    settings['instance'] = options['i']
-    var creat_tt = new (require('./CreatTT'))(settings);
-    creat_tt.start();
-}
-var ttmpSpider = function () {
-    var logger = logging.getLogger('ttmp',options['i'],log_level)
-    settings['logger'] = logger
-    settings['instance'] = options['i']
-    var toutiao = new (require('./toutiaoHome'))(settings);
-    toutiao.start();
-}
-var ttSpider = function () {
-    var logger = logging.getLogger('toutiao',options['i'],log_level)
-    settings['logger'] = logger
-    settings['instance'] = options['i']
-    var toutiao = new (require('./toutiao'))(settings);
-    toutiao.assembly()
-}
-var ntt = function () {
-    var logger = logging.getLogger('toutiao',options['i'],log_level)
-    settings['logger'] = logger
-    settings['instance'] = options['i']
-    var toutiao = new (require('./toutiao_new'))(settings);
-    toutiao.start()
-}
-var iqiyiFans = function () {
-    var logger = logging.getLogger('爱奇艺粉丝',options['i'],log_level);
-    settings['logger'] = logger;
-    settings['instance'] = options['i'];
-    var spider = new (require('./iqiyiFans'))(settings);
+var event = new EventEmitter();
 
-    spider.start();
-}
-var iqiyi = function () {
-    var logger = logging.getLogger('爱奇艺',options['i'],log_level);
-    settings['logger'] = logger;
-    settings['instance'] = options['i'];
-    var spider = new (require('./iqiyi'))(settings);
+var i = argv.i;
+var t = argv.t;
 
-    spider.start();
+if ( !i ) {
+    i = 'generate';
 }
-var souhu = function () {
-    var logger = logging.getLogger('搜狐',options['i'],log_level);
-    settings['logger'] = logger;
-    settings['instance'] = options['i'];
-    var spider = new (require('./souhu'))(settings);
+var way = path.join( '.' , '/instance' , i , 'settings.json' );
 
-    spider.start()
-}
-var ttbk = function () {
-    var logger = logging.getLogger('天天快报',options['i'],log_level);
-    settings['logger'] = logger;
-    settings['instance'] = options['i'];
-    var spider = new (require('./ttkb'))(settings);
+// TODO: Judge instance exist
+fs.exists( way , function ( result ) {
+    if ( result ) {
+        event.emit( 'exist' );
+    } else {
+        event.emit( 'non' );
+    }
+} );
 
-    spider.start()
-}
-////proxy Service////////////////////////////////////////////////////////////
-var proxyService = function(){
-	var logger = logging.getLogger('proxy-service',options['i'],log_level);
-	settings['logger'] = logger;
-	settings['port'] = parseInt(options['p']);
-	var proxyRouter = new (require('./proxyrouter'))(settings);
-	
-	proxyRouter.start();
-}
-////config service////////////////////////////////////////////////////////////
-var configService = function(){
-	var logger = logging.getLogger('config-service',options['i'],log_level);
-	settings['logger'] = logger;
-	settings['port'] = parseInt(options['p']);
-	var webConfig = new(require('./webconfig'))(settings);
-	
-	webConfig.start();	
-}
+event.on( 'exist' , function () {
 
-////route/////////////////////////////////////////////////////////////////////
-switch(options['a']){
-    case 'crawl':
-        crawling();
-        break;
-    case 'proxy':
-        proxyService();
-        break;
-    case 'config':
-        configService();
-        break;
-    case 'meipai':
-        meipai()
-        break
-    case 'youku':
-        youku()
-        break
-    case 'miaopai':
-        miaopai()
-        break
-    case 'bili':
-        bili()
-        break
-    case 'tx':
-        tx()
-        break
-    case "le":
-        le()
-        break
-    case 'creatTT':
-        creatTT()
-        break
-    case 'ttmp':
-        ttmpSpider()
-        break
-    case 'tt':
-        ttSpider()
-        break
-    case 'ntt':
-        ntt()
-        break
-    case 'iFans':
-        iqiyiFans()
-        break
-    case 'iqiyi':
-        iqiyi()
-        break
-    case 'souhu':
-        souhu()
-        break
-    case 'ttkb':
-        ttbk()
-        break
-    default:
-        userArgv.showHelp();
-}
+    var settings = require( './' + way );
+
+    settings.logger = logger.getLogger( i , i , 'trace' );
+
+    var scheduler = new (require( './scheduler' ))( settings );
+
+    if ( t ) {
+        scheduler.test();
+    } else {
+        scheduler.start();
+    }
+} );
+
+event.on( 'non' , function () {
+    console.error( 'Specify instance name config is not exist' , { path : way } );
+} );
---
+++ scheduler/index.js
@@ -0,0 +1,140 @@
+/**
+ * spider core
+ */
+var async = require( 'async' )
+var kue = require( 'kue' )
+var request = require( 'request' )
+var test_data = require('../data.json')
+
+var logger
+
+var scheduler = function ( settings ) {
+    'use strict'
+    this.settings = settings
+    logger = this.settings.logger
+    this.queue = kue.createQueue( {
+        redis : {
+            port : settings.port ,
+            host : settings.host ,
+            auth : settings.auth ,
+            db : settings.db
+        }
+    } )
+    logger.trace( '调度器初始化完成' )
+}
+scheduler.prototype.start = function () {
+    logger.trace('start')
+    this.getTask()
+}
+scheduler.prototype.getTask = function () {
+    request.get(this.settings.url,function (err,res,body) {
+        if(err){
+            logger.error()
+        }
+    })
+    
+}
+scheduler.prototype.wait = function () {
+    logger.debug("开始等待下次执行时间")
+    var self = this
+    setTimeout(function () {
+        var now = new Date()
+        if(now.getHours() == 2 || now.getHours() == 4){
+            self.test()
+        }else{
+            logger.debug("now",now.getHours())
+            self.wait()
+        }
+    },this.settings.interval)
+}
+scheduler.prototype.createQueue = function (raw,callback) {
+    var job = this.queue.create( raw.type , {
+        uid: raw.uid,
+        id: raw.id,
+        p: raw.p,
+        name: raw.name,
+        property: raw.property
+    }).priority('critical').attempts(5).backoff({
+        delay: 30*1000,
+        type: 'fixed'
+    }).removeOnComplete(true).ttl(60000).save(function (err) {
+        if(err){
+            logger.error( 'Create queue occur error' )
+            logger.info( 'error :' , err )
+        }
+        callback()
+    })
+}
+scheduler.prototype.deal = function ( raw, callback ) {
+    var self = this
+    var data = raw.data,
+        len = data.length,
+        i = 0
+    async.whilst(
+        function () {
+            return i < len
+        },
+        function (cb) {
+            var _ = data[i],processed,type
+            switch( _.p ){
+                case 1:
+                    type = "youku"
+                    break
+                case 2:
+                    type = "iqiyi"
+                    break
+                case 3:
+                    type = "le"
+                    break
+                case 4:
+                    type = "tencent"
+                    break
+                case 5:
+                    type = "meipai"
+                    break
+                case 6:
+                    type = "toutiao"
+                    break
+                case 7:
+                    type = "miaopai"
+                    break
+                case 8:
+                    type = "bili"
+                    break
+                case 9:
+                    type = "souhu"
+                    break
+                case 10:
+                    type = "kuaibao"
+                    break
+                default:
+                    break
+            }
+            processed = {
+                uid: raw.id,
+                id: _.id,
+                p: _.p,
+                name: _.name,
+                type: type,
+                property: _.property ? _.property : ''
+            }
+            self.createQueue(processed,function () {
+                i++
+                cb()
+            })
+        },
+        function () {
+            if(callback){
+                callback()
+            }
+            self.wait()
+        }
+    )
+}
+scheduler.prototype.test = function () {
+    logger.trace('测试模式')
+    var data = test_data
+    logger.debug(data)
+    this.deal(data)
+}
+module.exports = scheduler
