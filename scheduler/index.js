029400b165e61408c434d66f47e270cb5d8e7726
junhao <changjunhao@outlook.com>
2016-09-06 10:53:31 +0800

    new scheduler

--- scheduler/index.js
+++ scheduler/index.js
@@ -1,61 +1,47 @@
 /**
- * spider core
+ * Created by ifable on 16/9/2.
  */
 const async = require( 'async' )
 const kue = require( 'kue' )
 const request = require( 'request' )
 const myRedis = require( '../lib/myredis.js' )
-// const test_data = require('../data.json')
 
 let logger
-    
+
 class scheduler {
-    constructor ( settings ) {
+    constructor(settings) {
         this.settings = settings
         this.redis = settings.redis
         logger = this.settings.logger
-        this.queue = kue.createQueue( {
-            redis : {
-                port : this.redis.port ,
-                host : this.redis.host ,
-                auth : this.redis.auth ,
-                db : this.redis.jobDB
+        this.queue = kue.createQueue({
+            redis: {
+                port: this.redis.port,
+                host: this.redis.host,
+                auth: this.redis.auth,
+                db: this.redis.jobDB
             }
-        } )
-        logger.trace( '调度器初始化完成' )
-    }
-    assembly ( ) {
-        async.parallel([
-            (callback) => {
-                myRedis.createClient(this.redis.host,
-                    this.redis.port,
-                    this.redis.taskDB,
-                    this.redis.auth,
-                    ( err, cli ) => {
-                        if(err){
-                            return callback(err)
-                        }
-                        this.taskDB = cli
-                        logger.debug( "任务信息数据库连接建立...成功" )
-                        callback()
-                    }
-                )
-            }
-        ],(err, results) => {
-            if ( err ) {
-                logger.error( "连接redis数据库出错。错误信息：", err )
-                logger.error( "出现错误，程序终止。" )
-                process.exit()
-                return
-            }
-            logger.debug( '创建数据库连接完毕' )
-            setInterval( () => {
-                this.getTask()
-            }, 5000)
-            // setInterval( () => {
-            //     this.deal(test_data)
-            // }, 1000)
         })
+        logger.trace('调度器初始化完成')
+    }
+    assembly () {
+        myRedis.createClient(this.redis.host,
+            this.redis.port,
+            this.redis.taskDB,
+            this.redis.auth,
+            ( err, cli ) => {
+                if(err){
+                    logger.error( "连接redis数据库出错。错误信息：", err )
+                    logger.error( "出现错误，程序终止。" )
+                    process.exit()
+                    return
+                }
+                this.taskDB = cli
+                logger.debug( "任务信息数据库连接建立...成功" )
+                setInterval( () => {
+                    this.getTask()
+                }, 5000)
+            }
+        )
     }
     start () {
         logger.trace('启动函数')
@@ -78,9 +64,7 @@ class scheduler {
                 logger.info(body)
                 return
             }
-            this.deal_online(result, (err) => {
-
-            })
+            this.deal_online(result)
         })
     }
     createQueue (raw,callback) {
@@ -97,128 +81,46 @@ class scheduler {
                     name: raw.name,
                     encodeId: raw.encodeId,
                     type: raw.type
-                }).priority('critical').attempts(3).backoff({delay: 30*1000, type:'fixed'}).removeOnComplete(true)
-                    .save(function (err) {
-                        if(err){
-                            logger.error( 'Create queue occur error' )
-                            logger.info( 'error :' , err )
-                        }
-                        logger.debug("任务: " + job.type + "_" + job.data.id + " 创建完成")
-                        callback()
-                    })
+                }).priority('critical').attempts(5).backoff({delay: 30*1000, type:'fixed'}).removeOnComplete(true)
+                if( result.videoNumber != 0 ){
+                    job.ttl( Math.ceil(result.videoNumber / 500) * 210000 )
+                }
+                job.save(function (err) {
+                    if(err){
+                        logger.error( 'Create queue occur error' )
+                        logger.info( 'error :' , err )
+                    }
+                    logger.debug("任务: " + job.type + "_" + job.data.id + " 创建完成")
+                    callback()
+                })
             }else{
                 callback()
             }
         })
     }
-    deal ( raw, callback ) {
-        let data = raw.data,
-            len = data ? data.length : 0,
-            i = 0, _,processed,platform
-        //logger.debug(raw)
-        async.whilst(
-            () => {
-                return i < len
-            },
-            (cb) => {
-                _ = data[i]
-                switch( Number(_.p) ){//_.platform
-                    case 1:
-                        platform = "youku"
-                        break
-                    case 2:
-                        platform = "iqiyi"
-                        break
-                    case 3:
-                        platform = "le"
-                        break
-                    case 4:
-                        platform = "tencent"
-                        break
-                    case 5:
-                        platform = "meipai"
-                        break
-                    case 6:
-                        platform = "toutiao"
-                        break
-                    case 7:
-                        platform = "miaopai"
-                        break
-                    case 8:
-                        platform = "bili"
-                        break
-                    case 9:
-                        platform = "souhu"
-                        break
-                    case 10:
-                        platform = "kuaibao"
-                        break
-                    case 11:
-                        platform = "yidian"
-                        break
-                    case 12:
-                        platform = "tudou"
-                        break
-                    case 13:
-                        platform = "baomihua"
-                        break
-                    case 14:
-                        platform = "ku6"
-                        break
-                    case 15:
-                        platform = "btime"
-                        break
-                    case 16:
-                        platform = "weishi"
-                        break
-                    case 17:
-                        platform = "xiaoying"
-                        break
-                    case 18:
-                        platform = "budejie"
-                        break
-                    case 19:
-                        platform = "neihan"
-                        break
-                    case 20:
-                        platform = "yy"
-                        break
-                    default:
-                        break
-                }
-                // processed = {
-                //     uid: _.id,
-                //     id: _.bid,
-                //     p: _.platform,
-                //     name: _.bname,
-                //     platform: platform,
-                //     encodeId: _.encodeId ? _.encodeId : '',
-                //     type: _.type ? _.type : ''
-                // }
-                processed = {
-                    uid: raw.id,
-                    id: _.id,
-                    p: _.p,
-                    name: _.name,
-                    platform: platform,
-                    encodeId: _.encodeId ? _.encodeId : '',
-                    type: _.type ? _.type : ''
-                }
-                //logger.debug(processed)
-                this.createQueue(processed, (err) => {
-                    i++
-                    cb()
-                })
-            },
-            () => {
-                if(callback){
-                    callback()
-                }
-                //logger.debug("开始等待下次执行时间")
+    checkKey ( raw, callback ) {
+        let key = raw.p + ':' + raw.id,
+            time = new Date().getTime()
+        this.taskDB.hmget( key, 'id', 'video_number', ( err, result ) => {
+            if(err){
+                logger.debug(err)
+                return callback(err)
             }
-        )
+            if(result[0] === null){
+                this.taskDB.hmset( key, 'id', raw.id, 'init', time, 'create', time, 'video_number', 0)
+                return callback(null,{videoNumber: 0})
+            }
+            if(result[1] === null){
+                this.taskDB.hmset( key, 'create', time)
+                return callback(null,{videoNumber: 0})
+            }
+            if(result[1] >= 0){
+                this.taskDB.hmset( key, 'create', time)
+                return callback(null,{videoNumber: result[1]})
+            }
+        })
     }
-    deal_online ( raw, callback ) {
+    deal_online ( raw ) {
         let data = raw.data,
             len = data ? data.length : 0,
             i = 0, _,processed,platform
@@ -229,11 +131,7 @@ class scheduler {
             },
             (cb) => {
                 _ = data[i]
-                if( _.platform == 14 && _.bid == 24586950){
-                    i++
-                    return cb()
-                }
-                switch( Number(_.platform) ){//_.platform
+                switch( Number(_.platform) ){
                     case 1:
                         platform = "youku"
                         break
@@ -253,7 +151,7 @@ class scheduler {
                         platform = "toutiao"
                         break
                     case 7:
-                        platform = "miaopai"
+                        platform = "git p"
                         break
                     case 8:
                         platform = "bili"
@@ -313,73 +211,10 @@ class scheduler {
                 })
             },
             () => {
-                if(callback){
-                    callback()
-                }
                 //logger.debug("online 开始等待下次执行时间")
             }
         )
     }
-    checkKey ( raw, callback ) {
-        let key = raw.p + ':' + raw.id
-        this.taskDB.hmget( key, 'id', ( err, result ) => {
-            if(err){
-                logger.debug(err)
-                return callback(err)
-            }
-            if(result[0] === null){
-                this.setKey( raw, () => {
-                    return callback(null,true)
-                })
-            }else{
-                return callback(null,true)
-                // this.checkTime( raw, ( err, result ) => {
-                //     if(err){
-                //         return callback(err)
-                //     }
-                //     if(result){
-                //         return callback(null,true)
-                //     }
-                //     return callback(null,false)
-                // })
-            }
-        })
-    }
-    setKey ( raw, callback ) {
-        let key = raw.p + ':' + raw.id,
-            time = new Date().getTime()
-        this.taskDB.hmset( key, 'id', raw.id, 'init', time, 'create', time,
-            ( err, result ) => {
-                if(err){
-                    return callback(err)
-                }
-                if( result === 'OK' ){
-                    return callback(null,true)
-                }
-                return callback(null,false)
-            })
-    }
-    checkTime ( raw, callback ) {
-        let key = raw.p + ':' + raw.id,
-            time = new Date().getTime()
-        this.taskDB.hmget( key, 'update', 'init', 'create', ( err, result) => {
-            if(err){
-                return callback(err)
-            }
-            if(result[0] === null && time - result[2] >= 300000){
-                this.taskDB.hset( key, 'create', time)
-                return callback(null,true)
-            }
-            if(result[0] !== null && time - result[2] >= 300000 &&  result[2] > result[0] ){
-                this.taskDB.hset( key, 'create', time)
-                return callback(null,true)
-            }
-            if(result[0] !== null && time - result[2] >= 3600000){
-                this.taskDB.hset( key, 'create', time)
-                return callback(null,true)
-            }
-            return callback(null,false)
-        })
-    }
 }
+
 module.exports = scheduler
