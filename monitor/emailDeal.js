c180e86d62092f0094a3088c2b5ab11febe1a206
junhao <changjunhao@outlook.com>
2016-10-26 16:31:05 +0800

    pasue

--- monitor/emailDeal.js
+++ monitor/emailDeal.js
@@ -2,26 +2,28 @@
  * Created by ifable on 2016/10/6.
  */
 const nodemailer = require('nodemailer')
-//const transporter = nodemailer.createTransport('smtps://user%40gmail.com:pass@smtp.gmail.com')
-const transporter= nodemailer.createTransport({
-    service: 'QQex', // no need to set host or port etc.
-    auth: {
-        user: 'changjunhao@meimiao.net',
-        pass: 'Verona:2319446'
+
+exports.sendEmail = ( subject, content ) => {
+    const transporter= nodemailer.createTransport({
+        service: 'QQex', // no need to set host or port etc.
+        auth: {
+            user: 'changjunhao@meimiao.net',
+            pass: 'Verona:2319446'
+        }
+    })
+    const mailOptions = {
+        from: '"常君豪" <changjunhao@meimiao.net>', // sender address
+        to: 'changjunhao@meimiao.net', // list of receivers
+        subject: subject, // Subject line
+        text: content, // plaintext body
+        html: '<b>content</b>' // html body
     }
-})
-var mailOptions = {
-    from: '"常君豪" <changjunhao@meimiao.net>', // sender address
-    to: 'si_cheng_cool@126.com', // list of receivers
-    subject: 'Hello', // Subject line
-    text: 'Hello world', // plaintext body
-    html: '<b>Hello world！</b>' // html body
-};
 
 // send mail with defined transport object
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        return console.log(error);
-    }
-    console.log('Message sent: ' + info.response);
-});
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            return console.log(error);
+        }
+        console.log('Message sent: ' + info.response);
+    })
+}
