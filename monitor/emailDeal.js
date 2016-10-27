01ab979bf925cef28b9fa2aa172dce82939f7985
junhao <changjunhao@outlook.com>
2016-10-27 11:32:31 +0800

    email server end

--- monitor/emailDeal.js
+++ monitor/emailDeal.js
@@ -3,20 +3,20 @@
  */
 const nodemailer = require('nodemailer')
 
+const transporter= nodemailer.createTransport({
+    service: 'QQex', // no need to set host or port etc.
+    auth: {
+        user: 'changjunhao@meimiao.net',
+        pass: 'Verona:2319446'
+    }
+})
 exports.sendEmail = ( subject, content ) => {
-    const transporter= nodemailer.createTransport({
-        service: 'QQex', // no need to set host or port etc.
-        auth: {
-            user: 'changjunhao@meimiao.net',
-            pass: 'Verona:2319446'
-        }
-    })
     const mailOptions = {
         from: '"常君豪" <changjunhao@meimiao.net>', // sender address
         to: 'changjunhao@meimiao.net', // list of receivers
         subject: subject, // Subject line
         text: content, // plaintext body
-        html: '<b>content</b>' // html body
+        html: `<b>${content}</b>` // html body
     }
 
 // send mail with defined transport object
