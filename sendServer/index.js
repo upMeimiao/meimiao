27c2a9a34340fe46034f91c4e9a8e786ad15904c
junhao <changjunhao@outlook.com>
2016-10-27 17:04:44 +0800

    debug

--- sendServer/index.js
+++ sendServer/index.js
@@ -12,7 +12,7 @@ class sendServer {
         this.redis = settings.redis
         logger = settings.logger
         this.option = {
-            url: this.settings.url
+            url: this.settings.sendUrl
         }
         logger.trace('sendServer instantiation ...')
     }
