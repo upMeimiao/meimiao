dacc57fc6ec0cef92862ad21701a4d3c4ab60740
junhao <changjunhao@outlook.com>
2016-11-14 12:11:30 +0800

    debug send staging

--- sendServer/index.js
+++ sendServer/index.js
@@ -33,6 +33,9 @@ class sendServer {
                 setInterval(()=>{
                     this.emit('get_lists')
                 },20)
+                setInterval(()=>{
+                    this.emit('get_lists_staging')
+                },2000)
             }
         )
     }
@@ -41,13 +44,15 @@ class sendServer {
         this.on( 'get_lists', () => {
             this.deal()
         })
+        this.on( 'get_lists_staging', () => {
+            this.deal_staging()
+        })
         this.on( 'send_data', ( raw ) => {
             this.send( raw )
+        })
+        this.on( 'send_data_staging', ( raw ) => {
             this.send_staging( raw )
         })
-        // this.on( 'send_data_staging', ( raw ) => {
-        //     this.send_staging( raw )
-        // })
         this.assembly()
     }
     deal () {
