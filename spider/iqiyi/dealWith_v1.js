b5aab4c64578b859445a8a790d5d416ccd60b1a8
junhao <changjunhao@outlook.com>
2016-11-15 15:43:06 +0800

    iqiqi v2

--- spider/iqiyi/dealWith_v1.js
+++ spider/iqiyi/dealWith_v1.js
@@ -1,25 +1,23 @@
 /**
- * Created by ifable on 2016/11/15.
+ * Created by junhao on 16/6/22.
  */
 const async = require( 'async' )
 const cheerio = require('cheerio')
-const request = require( '../../lib/request' )
+const request = require( '../lib/req_iqiyi' )
 let logger
 const jsonp = function (data) {
     return data
 }
-
 class dealWith {
     constructor(spiderCore) {
         this.core = spiderCore
         this.settings = spiderCore.settings
-        this.api = spiderCore.settings.spiderAPI.iqiyi
         logger = this.settings.logger
         logger.trace('DealWith instantiation ...')
     }
     todo ( task, callback ) {
         task.total = 0
-        async.parallel(
+        async.series(
             {
                 user: (callback) => {
                     this.getUser(task,(err)=>{
@@ -27,7 +25,7 @@ class dealWith {
                     })
                 },
                 media: (callback) => {
-                    this.getList( task, ( err ) => {
+                    this.getTotal( task, ( err ) => {
                         if(err){
                             return callback(err)
                         }
@@ -45,23 +43,28 @@ class dealWith {
         )
     }
     getUser ( task, callback ){
-        const option = {
+        let option = {
             url: `http://m.iqiyi.com/u/${task.id}`,
             referer: `http://m.iqiyi.com/u/${task.id}`,
-            ua: 2
+            ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
         }
-        request.get( logger, option, ( err, result ) => {
+        request.get( option, ( err, result ) => {
             if(err){
+                logger.error( 'occur error : ', err )
                 return callback(err)
             }
-            const $ = cheerio.load(result.body),
+            if(result.statusCode != 200){
+                logger.error( '获取爱奇艺视频粉丝状态码错误' )
+                return callback(true)
+            }
+            let $ = cheerio.load(result.body),
                 fansDom = $('span.c-num-fans')
             if(fansDom.length === 0){
                 return this.get_user(task,function () {
                     callback()
                 })
             }
-            const fans = fansDom.attr('data-num'),
+            let fans = fansDom.attr('data-num'),
                 user = {
                     platform: 2,
                     bid: task.id,
@@ -74,14 +77,19 @@ class dealWith {
         })
     }
     get_user ( task, callback) {
-        const option = {
+        let option = {
             url: `http://m.iqiyi.com/u/${task.id}/fans`,
             referer: `http://m.iqiyi.com/u/${task.id}`,
-            ua: 2
+            ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
         }
-        request.get( logger, option, ( err, result ) => {
+        request.get( option, ( err, result ) => {
             if(err){
+                logger.error( 'occur error : ', err )
                 return callback(err)
+            }
+            if(result.statusCode != 200){
+                logger.error( '获取爱奇艺视频粉丝状态码错误' )
+                return callback(true)
             }
             let $ = cheerio.load(result.body),
                 fansDom = $('h3.tle').text(),
@@ -97,73 +105,147 @@ class dealWith {
         })
     }
     sendUser ( user, callback ){
-        const option = {
-            url: this.settings.sendFans,
+        let option = {
+            url: this.settings.sendToServer[0],
             data: user
         }
-        request.post( logger, option, (err,result)=>{
+        request.post(option, (err,result)=>{
             if(err){
+                logger.error( 'occur error : ', err )
+                logger.info(`返回爱奇艺用户 ${user.bid} 连接服务器失败`)
                 return callback(err)
             }
             try{
                 result = JSON.parse(result.body)
             }catch (e){
                 logger.error(`爱奇艺用户 ${user.bid} json数据解析失败`)
-                logger.error(result)
+                logger.info(result)
                 return callback(e)
             }
             if(result.errno == 0){
                 logger.debug("爱奇艺用户:",user.bid + ' back_end')
             }else{
                 logger.error("爱奇艺用户:",user.bid + ' back_error')
-                logger.error(result)
-                logger.error(`user info: `,user)
+                logger.info(result)
+                logger.info(`user info: `,user)
             }
             callback()
         })
     }
-    getList ( task, callback ) {
-        let sign = true,
-            index = 1
-        const option = {
-            ua: 1
+    getTotal ( task, callback ) {
+        let option,
+            page
+        option = {
+            url: this.settings.list[0] + task.id + "&page=" + 1,
+            host: 'www.iqiyi.com',
+            referer: this.settings.list[1] + task.id + "/v",
+            ua:'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'
         }
+        request.get( option, ( err, result ) => {
+            if(err){
+                logger.error( 'occur error : ', err )
+                return callback(err)
+            }
+            if(result.statusCode != 200){
+                logger.error( '获取爱奇艺视频总数状态码错误' )
+                return callback(true)
+            }
+            try {
+                result = JSON.parse(result.body)
+            } catch (e) {
+                logger.error('json数据解析失败')
+                logger.info(result)
+                return callback(e)
+            }
+            page = result.total
+            task.total = page * 42
+            if(page == 0){
+                option = {
+                    url: this.settings.list[1] + task.id + "/v"
+                }
+                request.get( option,(err,result) => {
+                    if(err){
+                        logger.error('occur error: ',err)
+                        return callback(err)
+                    }
+                    if(result.statusCode != 200){
+                        logger.error( '获取爱奇艺视频总数状态码错误' )
+                        return callback(true)
+                    }
+                    let $ = cheerio.load(result.body),
+                        username = $('.pf_username .username').text()
+                    if(username == ''){
+                        let totalText = $('.num_item a').text(),
+                            text = totalText.replace(/,/ig,'').split('视频'),
+                            total = text[0]
+                        task.total = total
+                        if(total % 42 == 0){
+                            page = total / 42
+                        }else{
+                            page = Math.ceil(total / 42)
+                        }
+                        this.getVideos( task, page, (err) => {
+                            if(err){
+                                return callback(err)
+                            }
+                            return callback()
+                        })
+                    }
+                })
+            }else{
+                this.getVideos( task, page, (err) => {
+                    if(err){
+                        return callback(err)
+                    }
+                    callback()
+                })
+            }
+        })
+
+    }
+    getVideos ( task, page, callback ) {
+        let index = 1,
+            option = {
+                host: 'www.iqiyi.com'
+            }
         async.whilst(
             () => {
-                return sign
+                return index <= page
             },
             ( cb ) => {
-                option.url = this.api.list[0] + task.id + "&page=" + index
+                option.url = this.settings.list[0] + task.id + "&page=" + index
                 option.referer = 'http://www.iqiyi.com/u/' + task.id + "/v"
-                request.get( logger, option, (err,result) => {
+                request.get( option, (err,result) => {
                     if(err){
+                        logger.error( 'occur error : ', err )
+                        return cb()
+                    }
+                    if(result.statusCode != 200){
+                        logger.error( '获取爱奇艺视频列表错误,第'+ index + '页' )
                         return cb()
                     }
                     try {
                         result = JSON.parse(result.body)
                     } catch (e) {
                         logger.error('json数据解析失败')
-                        logger.error(result)
-                        return cb()
+                        logger.info(result)
+                        return callback(e)
                     }
-                    const data = result.data,
+                    let data = result.data,
                         $ = cheerio.load(data,{
                             ignoreWhitespace:true
-                        })
-                    if($('.wrap-customAuto-ht li').length === 0){
-                        task.total = 42 * ( index - 1 )
-                        sign = false
-                        return cb()
-                    }
-                    const lis = $('li[tvid]'),ids = [],
+                        }),
+                        lis = $('li[tvid]'),ids = [],
                         ats = $('a[data-title]'),titles = [],
                         href = $('.site-piclist_info a[title]'),links = []
                     for(let i = 0 ;i<lis.length;i++){
-                        ids.push(lis[i].attribs.tvid.replace(/,/g,''))
+                        let id = lis[i].attribs.tvid.replace(/,/g,'')
+                        ids.push(id)
                     }
                     //logger.debug(ids)
                     for(let j = 0;j<ats.length;j++){
-                        titles.push(ats[j].attribs['data-title'])
+                        let title = ats[j].attribs['data-title']
+                        titles.push(title)
                     }
                     //logger.debug(titles)
                     for(let z = 0 ;z<href.length;z++){
@@ -183,7 +265,7 @@ class dealWith {
             }
         )
     }
-    deal( task, ids, titles, links, callback ){
+    deal ( task, ids, titles, links, callback ) {
         let index = 0,
             length = ids.length
         async.whilst(
@@ -200,7 +282,8 @@ class dealWith {
                     if(err){
                         //setTimeout(cb,600)
                         index++
-                        return cb()
+                        cb()
+                        return
                     }
                     index++
                     cb()
@@ -214,7 +297,7 @@ class dealWith {
     }
     info ( task, info, callback ) {
         let id = info.id, title = info.title,link = info.link
-        async.parallel(
+        async.series(
             [
                 (callback) => {
                     this.getInfo(id,link, (err,data) => {
@@ -233,15 +316,6 @@ class dealWith {
                             callback(null,data)
                         }
                     })
-                },
-                (callback) => {
-                    this.getPlay(id,link, (err,data) => {
-                        if(err){
-                            callback(err)
-                        } else {
-                            callback(null,data)
-                        }
-                    })
                 }
             ], (err,result) => {
                 if(err){
@@ -254,13 +328,12 @@ class dealWith {
                     aid: id,
                     title: title,
                     desc: result[0].desc,
-                    play_num: result[2],
+                    play_num: result[0].play,
                     support: result[1].data.up,
                     step: result[1].data.down,
                     comment_num: result[0].comment,
                     a_create_time: result[0].time
                 }
-                logger.debug(media)
                 this.sendCache( media )
                 callback()
             }
@@ -268,18 +341,24 @@ class dealWith {
     }
     getInfo ( id, link, callback ) {
         let option = {
-            url: this.api.info + id + "?callback=jsonp&status=1",
-            referer: link,
-            ua: 1
+            url: this.settings.info + id + "?callback=jsonp&status=1",
+            host: 'mixer.video.iqiyi.com',
+            referer: link
         }
-        request.get( logger, option, (err,result) => {
+        request.get( option, (err,result) => {
             if(err){
+                logger.error( 'occur error : ', err )
                 return callback(err)
             }
+            if(result.statusCode != 200){
+                logger.error( '获取爱奇艺视频总数状态码错误' )
+                return callback(true)
+            }
+            result = result.body.replace(/try{/g,'').replace(/;/g,'').replace(/}catch\(e\)\{\}/g,'')
             //logger.debug(backData)
             let playData
             try {
-                playData = eval(result.body)
+                playData = eval("("+result+")")
             } catch (e){
                 logger.error('eval错误:',e)
                 logger.error(result)
@@ -289,11 +368,13 @@ class dealWith {
                 return callback(true)
             }
             let name = playData.data.user.name,
+                playNum = playData.data.playCount,
                 desc = playData.data.description,
                 comment = playData.data.commentCount,
                 creatTime = parseInt(playData.data.issueTime / 1000),
                 data = {
                     name: name,
+                    play: playNum,
                     desc: desc,
                     comment: comment,
                     time: creatTime
@@ -302,19 +383,24 @@ class dealWith {
         })
     }
     getExpr ( id, link, callback ) {
-        const option = {
-            url: this.api.expr + id,
-            referer: link,
-            ua: 1
+        let option = {
+            url: this.settings.expr + id,
+            host: 'up.video.iqiyi.com',
+            referer: link
         }
-        request.get( logger, option, (err,result) => {
+        request.get( option, (err,result) => {
             if(err){
                 return callback(err)
             }
+            if(result.statusCode != 200){
+                logger.error( '获取爱奇艺视频扩展信息状态码错误' )
+                return callback(true)
+            }
+            result = result.body.replace(/try{/g,'').replace(/}catch\(e\)\{\}/g,'')
             //logger.debug(result)
             let infoData
             try {
-                infoData = eval(result.body)
+                infoData = eval("("+result+")")
             } catch (e){
                 logger.error('eval错误:',e)
                 logger.error(result)
@@ -324,28 +410,6 @@ class dealWith {
                 return callback(true)
             }
             callback(null,infoData)
-        })
-    }
-    getPlay ( id, link, callback ) {
-        const option = {
-            url: this.api.play + id + '/?callback=jsonp',
-            referer: link,
-            ua: 1
-        }
-        request.get( logger, option, (err,result) => {
-            if(err){
-                return callback(err)
-            }
-            //logger.debug(result)
-            let infoData
-            try {
-                infoData = eval(result.body)
-            } catch (e){
-                logger.error('eval错误:',e)
-                logger.error(result)
-                return callback(e)
-            }
-            callback(null,infoData[0][id])
         })
     }
     sendCache ( media ){
