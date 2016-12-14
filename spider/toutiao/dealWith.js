260065b0364a89bf83e4514203783ba47e405d2c
junhao <changjunhao@outlook.com>
2016-12-14 18:40:09 +0800

    debug toutaio

--- spider/toutiao/dealWith.js
+++ spider/toutiao/dealWith.js
@@ -67,6 +67,7 @@ class dealWith {
             ua: 3,
             own_ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92 NewsArticle/5.9.0.5 JsSdk/2.0 NetType/WIFI (News 5.9.0 10.200000)'
         }
+        logger.debug('user: ',option)
         request.get( logger, option, ( err, result ) => {
             if(err){
                 return callback(err)
@@ -150,6 +151,7 @@ class dealWith {
             option = {
                 ua: 3,
                 xrw: 1,
+                refere: `https://lf.snssdk.com/2/user/profile/v3/?media_id=${task.user_id}&to_html=1`,
                 own_ua: 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92 NewsArticle/5.9.0.5 JsSdk/2.0 NetType/WIFI (News 5.9.0 10.200000)'
             },
             hot_time = null
@@ -164,6 +166,7 @@ class dealWith {
                 }else{
                     option.url = this.settings.newList + task.id + '&media_id=' + task.user_id + '&cp=' + cp + "&as=" + as + "&max_behot_time="
                 }
+                logger.debug(option)
                 request.get( logger, option, (err,result) => {
                     if(err){
                         index++
