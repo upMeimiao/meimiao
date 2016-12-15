3fd7ac80b85bfed68ebc43f83ea4a045170b1ae4
junhao <changjunhao@outlook.com>
2016-12-15 17:16:01 +0800

    proxy module end

---
+++ proxy/getProxy.js
@@ -0,0 +1,41 @@
+const request = require( 'request' )
+
+let logger,settings
+class getProxy{
+    constructor(proxy) {
+        settings = proxy.settings
+        logger = settings.logger
+        logger.debug( '获取代理信息模块实例化...' )
+    }
+    ready(callback) {
+        this.get( (err, raw) => {
+            if(err){
+                return callback(err)
+            }
+            return callback(null, raw)
+        })
+    }
+    get(callback) {
+        const proxy = []
+        request(settings.proxy.api, (err, res, body) => {
+            if(err){
+                logger.error('Get proxy occur error')
+                return callback(err)
+            }
+            try{
+                body = JSON.parse(body)
+            } catch (e) {
+                logger.error('parse proxy-json  error')
+                return callback(e)
+            }
+            if(body.error){
+                return callback(body.error)
+            }
+            body.forEach((item) => {
+                proxy.push(item.host + ':' + item.port)
+            })
+            return callback(null, proxy)
+        })
+    }
+}
+module.exports = getProxy
