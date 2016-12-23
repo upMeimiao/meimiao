37c312f79e3420560e6017d546fd5277b8f38670
junhao <changjunhao@outlook.com>
2016-12-23 22:13:14 +0800

    wangyi debug

--- spider/wangyi/dealWith.js
+++ spider/wangyi/dealWith.js
@@ -196,41 +196,25 @@ class dealWith {
         )
     }
     getVideo(task ,data ,callback ) {
-        let time,a_create_time,media
-        if(data.videoinfo){
-            time = data.ptime
-            a_create_time = moment(time).format('X')
-            media = {
-                author: task.name,
-                platform: 25,
-                bid: task.id,
-                aid: data.videoID,
-                title: data.title.substr(0,100),
-                desc: data.digest.substr(0,100),
-                play_num: data.videoinfo.playCount,
-                comment_num: data.videoinfo.replyCount,
-                a_create_time: a_create_time,
-                v_img:data.imgsrc,
-                long_t:data.videoinfo.length,
-                class:data.TAGS
-            }
-        }else{
-            time = data.ptime
-            a_create_time = moment(time).format('X')
-            media = {
-                author: data.videoTopic.tname,
-                platform: task.p,
-                bid: task.id,
-                aid: data.videoID,
-                title: data.title.substr(0,100),
-                desc: data.digest.substr(0,100),
-                //play_num: data.playCount,
-                comment_num: data.replyCount,
-                a_create_time: a_create_time,
-                v_img:data.imgsrc,
-                long_t:data.length,
-                class:data.TAGS
-            }
+        if(!data.videoinfo){
+            return callback()
+        }
+        let media = {
+            author: task.name,
+            platform: 25,
+            bid: task.id,
+            aid: data.videoID,
+            title: data.title.substr(0,100),
+            desc: data.digest.substr(0,100),
+            play_num: data.videoinfo.playCount,
+            comment_num: data.videoinfo.replyCount,
+            a_create_time: moment(data.ptime).format('X'),
+            v_img:data.imgsrc,
+            long_t:data.videoinfo.length,
+            class:data.TAGS
+        }
+        if(!media.comment_num){
+            delete media.comment_num
         }
         this.sendCache( media )
         callback()
