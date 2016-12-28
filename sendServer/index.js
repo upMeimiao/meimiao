3e78e06e088f14ac38e35b1d24dfd3c21a2ee7bd
junhao <changjunhao@outlook.com>
2016-12-28 11:21:47 +0800

    debug send

--- sendServer/index.js
+++ sendServer/index.js
@@ -35,7 +35,7 @@ class sendServer {
         this.emit('get_lists')
         setInterval(() => {
             this.emit('get_lists')
-        }, 1000)
+        }, 800)
     }
     start () {
         logger.trace('启动函数')
@@ -56,7 +56,7 @@ class sendServer {
     }
     getData() {
         const key = [],list = []
-        for( let i = 0; i < 150; i++){
+        for( let i = 0; i < 100; i++){
             key[i] = ['lpop', 'cache']
         }
         this.redis.pipeline(
@@ -88,7 +88,7 @@ class sendServer {
                     this.send(list[i], 0)
                     i++
                     cb()
-                }, 5)
+                }, 3)
             }
         )
     }
