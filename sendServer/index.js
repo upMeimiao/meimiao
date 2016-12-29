92348b6988e05b957430375b1b94e3ba3e300c63
junhao <changjunhao@outlook.com>
2016-12-29 16:18:12 +0800

    send debug

--- sendServer/index.js
+++ sendServer/index.js
@@ -17,7 +17,7 @@ class sendServer {
             }
         }
         this.stagingOption = {
-            url: 'http://staging-dev.caihongip.com/index.php/Spider/video/postVideosMore/',
+            url: 'http://10.163.174.70/index.php/Spider/video/postVideosMore/',
             headers: {
                 'Content-Type':'application/x-www-form-urlencoded'
             },
