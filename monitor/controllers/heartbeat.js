68955d0736cf5ace1bc8b37258a31d960d1415d4
[zhupenghui] <[zhupenghui@meimiao.net]>
2017-01-12 19:09:46 +0800

    new-fengxing

--- monitor/controllers/heartbeat.js
+++ monitor/controllers/heartbeat.js
@@ -1,6 +1,6 @@
 const Redis = require('ioredis')
 
-const redis = new Redis(`redis://:C19prsPjHs52CHoA0vm@192.168.1.73:6379/3`,{
+const redis = new Redis(`redis://:C19prsPjHs52CHoA0vm@r-m5e43f2043319e64.redis.rds.aliyuncs.com:6379/3`,{
     reconnectOnError: function (err) {
         if (err.message.slice(0, 'READONLY'.length) === 'READONLY') {
             return true
