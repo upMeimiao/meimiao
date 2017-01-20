1aeb58dcf3f78eca3817d98e134fc917a7fd2082
junhao <changjunhao@outlook.com>
2017-01-20 17:56:26 +0800

    fixed toutiao bug

--- servant/dealWith.js
+++ servant/dealWith.js
@@ -517,7 +517,7 @@ class DealWith {
                 }
                 callback(null,res)
             })
-        }else if(pathname.startsWith('/a')){
+        }else if(pathname.startsWith('/a') || pathname.startsWith('/group/')){
             r.head(data,{headers:{'User-Agent':':Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'}},(err,res,body)=>{
                 v_id = (res.request.path).replace(/\//g,'').substring(1)
                 option.url = api.toutiao.url + v_id + "/info/"
