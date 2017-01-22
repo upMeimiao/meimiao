a4fed3b7bac94b440565d0e7c80e78b8464f707a
[zhupenghui] <[zhupenghui@meimiao.net]>
2017-01-22 11:07:33 +0800

    new-mgtv 字段修改

--- spider/mgtv/dealWith.js
+++ spider/mgtv/dealWith.js
@@ -136,7 +136,7 @@ class dealWith {
                 desc: result[3] ? result[3].substring(0,100).replace(/"/g,'') : '',
                 comment_num: result[5].total_number
             }
-            logger.debug(media.desc)
+            logger.debug(media)
             this.sendCache( media )
             callback()
         })
@@ -160,6 +160,9 @@ class dealWith {
                 logger.error('数据解析失败')
                 return
             }
+            if(!result.total_number){
+                result.total_number = ''
+            }
             callback(null,result)
         })
     }
@@ -181,6 +184,12 @@ class dealWith {
             } catch(e){
                 logger.error('数据解析失败')
                 return
+            }
+            if(!result.data){
+                result.data = {
+                    like: '',
+                    unlike: ''
+                }
             }
             callback(null,result)
         })
@@ -220,6 +229,11 @@ class dealWith {
                 logger.error('数据解析失败')
                 return
             }
+            if(!result.data.fstlvlName){
+                result.data = {
+                    fstlvlName: ''
+                }
+            }
             callback(null,result.data)
         })
     }
@@ -243,6 +257,9 @@ class dealWith {
                 logger.error('数据解析失败')
                 return
             }
+            if(!result.data.all){
+                result.data.all = ''
+            }
             callback(null,result.data)
         })
     }
@@ -250,7 +267,7 @@ class dealWith {
         let option = {
             url: this.settings.videoInfo + video.video_id
         }
-        logger.debug(option.url)
+        //logger.debug(option.url)
         request.get( logger, option, ( err, result ) => {
             if(err){
                 logger.debug('单个视频请求失败 ' + err)
@@ -265,6 +282,11 @@ class dealWith {
             } catch(e){
                 logger.error('数据解析失败')
                 return
+            }
+            if(!result.info.duration || !result.info){
+                result.info = {
+                    duration: ''
+                }
             }
             callback(null,result.data)
         })
--- spider/mgtv/index.js
+++ spider/mgtv/index.js
@@ -59,11 +59,23 @@ class spiderCore {
             }
             logger.debug( '创建数据库连接完毕' )
             this.deal()
+            //this.test()
         })
     }
     start () {
         logger.trace('启动函数')
         this.assembly()
+    }
+    test () {
+        let work = {
+            id: 292208,
+            name: '微在不懂爱',
+            p: 27
+        }
+        this.dealWith.todo( work, (err, total) => {
+            logger.debug(total)
+            logger.debug('end')
+        })
     }
     deal () {
         let queue = kue.createQueue({
