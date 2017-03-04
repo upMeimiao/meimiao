bdc8d51690885385ff69e69ee6c9ed8fec657e70
junhao <changjunhao@outlook.com>
2017-03-04 18:23:33 +0800

    添加spider服务器

--- shell/cleanAll.sh
+++ shell/cleanAll.sh
@@ -4,12 +4,16 @@ scheduler_1="10.251.55.50"
 scheduler_2="10.169.16.235"
 send_1="10.28.79.123"
 send_2="10.28.79.37"
+spider_0="10.169.22.212"
 spider_1="10.163.223.12"
 spider_2="10.163.216.52"
-spider_3="10.169.22.212"
-spider_4="10.161.93.13"
-spider_5="10.252.29.87"
-spider_6="10.144.191.122"
+spider_3="10.28.227.35"
+spider_4="10.28.227.72"
+spider_5="10.161.93.13"
+spider_6="10.252.29.87"
+spider_7="10.28.227.41"
+spider_8="10.144.191.122"
+spider_9="10.30.144.86"
 
 echo "--------------中控服务器log 清理--------------"
 echo
@@ -17,7 +21,7 @@ echo "--------------$scheduler_1--------------"
 cd ~/qiaosuan && sh clean.sh
 echo "-------------- $scheduler_1 log 清理完成--------------"
 echo
-for loop in ${scheduler_2} ${send_1} ${send_2} ${spider_1} ${spider_2} ${spider_3} ${spider_4} ${spider_5} ${spider_6}
+for loop in ${scheduler_2} ${spider_0} ${send_1} ${send_2} ${spider_1} ${spider_2} ${spider_3} ${spider_4} ${spider_5} ${spider_6} ${spider_7} ${spider_8} ${spider_9}
 do
     echo
     echo "--------------$loop--------------"
--- shell/reload.sh
+++ shell/reload.sh
@@ -4,12 +4,16 @@ scheduler_1="10.251.55.50"
 scheduler_2="10.169.16.235"
 send_1="10.28.79.123"
 send_2="10.28.79.37"
+spider_0="10.169.22.212"
 spider_1="10.163.223.12"
 spider_2="10.163.216.52"
-spider_3="10.169.22.212"
-spider_4="10.161.93.13"
-spider_5="10.252.29.87"
-spider_6="10.144.191.122"
+spider_3="10.28.227.35"
+spider_4="10.28.227.72"
+spider_5="10.161.93.13"
+spider_6="10.252.29.87"
+spider_7="10.28.227.41"
+spider_8="10.144.191.122"
+spider_9="10.30.144.86"
 
 echo $1
 if [ $1 == 'sc' ]
@@ -110,7 +114,7 @@ then
     esac
 elif [ $1 == 'sp' ]
 then
-    for loop in ${spider_1} ${spider_2} ${spider_3} ${spider_4} ${spider_5} ${spider_6}
+    for loop in ${spider_0} {spider_1} ${spider_2} ${spider_3} ${spider_4} ${spider_5} ${spider_6} ${spider_7} ${spider_8} ${spider_9}
         do
             echo
             echo "--------------$loop--------------"
