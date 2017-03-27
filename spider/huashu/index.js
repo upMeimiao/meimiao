8027fe7c6b0668134afd895cc5a96c795847deef
zhugazi <zhu_gazi@163.com>
2017-03-27 14:39:19 +0800

    '华数debug'

--- spider/huashu/index.js
+++ spider/huashu/index.js
@@ -69,8 +69,8 @@ class spiderCore {
     test(){
         let work = {
             p:35,
-            name:'四叶草影视',
-            id:40307
+            name:'十月呵护',
+            id:62946
         }
         this.dealWith.todo(work, (err,total,uid) => {
             logger.debug(total)
