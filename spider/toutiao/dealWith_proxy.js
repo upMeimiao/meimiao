1325c4907244186fdfd451a79b59e0f94eec477a
junhao <changjunhao@outlook.com>
2017-04-01 11:10:16 +0800

    debug toutiao

--- spider/toutiao/dealWith_proxy.js
+++ spider/toutiao/dealWith_proxy.js
@@ -20,12 +20,13 @@ class dealWith {
         const t = Math.floor((new Date).getTime() / 1e3),
             e = t.toString(16).toUpperCase(),
             n = md5(t.toString()).toString().toUpperCase()
-        if (8 != e.length) return {
+        if (8 !== e.length) return {
             as: "479BB4B7254C150",
             cp: "7E0AC8874BB0985"
         }
-        for (var o = n.slice(0, 5), i = n.slice(-5), a = "", r = 0; 5 > r; r++) a += o[r] + e[r]
-        for (var l = "", s = 0; 5 > s; s++) l += e[s + 3] + i[s]
+        let o,l,i,a,r,s
+        for (o = n.slice(0, 5), i = n.slice(-5), a = "", r = 0; 5 > r; r++) a += o[r] + e[r]
+        for (l = "", s = 0; 5 > s; s++) l += e[s + 3] + i[s]
         return {
             as: "A1" + a + e.slice(-3),
             cp: e.slice(0, 3) + l + "E1"
