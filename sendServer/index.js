44d78a5ae9ce07e6592165c19e73e54b9e949ac4
junhao <changjunhao@outlook.com>
2017-04-01 16:28:39 +0800

    fixed send bug

--- sendServer/index.js
+++ sendServer/index.js
@@ -13,7 +13,7 @@ class sendServer {
         this.onlineOption = {
             url: "http://100.98.39.12/index.php/Spider/video/postVideosMore/",//settings.sendUrl,
             headers: {
-                "content-type": "application/json"
+                // "content-type": "application/json"
             }
         }
         this.stagingOption = {
@@ -78,7 +78,7 @@ class sendServer {
                }
             }
             this.emit('send_data', list, 0)
-            this.emit('send_data_staging', list, 0)
+            // this.emit('send_data_staging', list, 0)
         })
     }
     sendOnline(list, time){
@@ -102,8 +102,8 @@ class sendServer {
             return
         }
         // this.onlineOption.body = JSON.stringify({data: list})
-        // this.onlineOption.form = {data: newList}
-        this.onlineOption.body = JSON.stringify({data: newList})
+        this.onlineOption.form = {data: newList}
+        // this.onlineOption.body = JSON.stringify({data: newList})
         request.post(this.onlineOption, (err, res, result) => {
             if(err){
                 logger.error('online occur error : ', err.message)
