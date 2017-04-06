439c493f05543b0b673cc124f80c30460a7ad55d
junhao <changjunhao@outlook.com>
2017-04-06 20:54:36 +0800

    fixed youtube desc bug

--- spider/youtube/dealWith.js
+++ spider/youtube/dealWith.js
@@ -305,7 +305,7 @@ function _desc(raw) {
     if(!raw || raw.length === 0 || !raw[0].description || !raw[0].description.runs || raw[0].description.runs.length === 0 || !raw[0].description.runs[0].text){
         return ''
     }
-    return spiderUtils.stringHandling(raw[0].description.runs[0].text)
+    return spiderUtils.stringHandling(raw[0].description.runs[0].text, 100)
 }
 function _class(raw) {
     let str = ''
