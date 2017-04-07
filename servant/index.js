064944c39153844adb8d8281efbf0fec706b6eeb
junhao <changjunhao@outlook.com>
2017-04-07 15:35:54 +0800

    debug

--- servant/index.js
+++ servant/index.js
@@ -31,8 +31,9 @@ class spiderCore {
     }
     getHandle(req, res){
         if (req.url === '/favicon.ico' || URL.parse(req.url).pathname !== '/'){
+            logger.debug(URL.parse(req.url).pathname)
             res.writeHead(404,{'Content-Type': 'text/html;charset=utf-8'});
-            res.end('dd');
+            res.end(URL.parse(req.url).pathname);
             return
         }
         this.dispatch( req, res )
