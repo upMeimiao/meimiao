788945cab96df9d7fe3f8c87421286a79b64c48d
junhao <changjunhao@outlook.com>
2017-04-20 16:08:53 +0800

    add dianshi

--- spider/dianshi/dealWith.js
+++ spider/dianshi/dealWith.js
@@ -1,7 +1,6 @@
 /**
  * Created by penghui on 17/4/17.
  */
-const moment = require('moment');
 const async = require('async');
 const request = require('../../lib/request');
 const req = require('request');
@@ -13,53 +12,56 @@ class dealWith {
     this.core = spiderCore;
     this.settings = spiderCore.settings;
     logger = this.settings.logger;
-    logger.trace('DealWith instantiation ...')
+    logger.trace('DealWith instantiation ...');
   }
   todo(task, callback) {
     task.total = 0;
     task.page = 0;
     this.getTotal(task, (err) => {
       if (err) {
-        return callback(err)
+        callback(err);
+        return;
       }
       callback(null, task.total);
-    })
+    });
   }
   getTotal(task, callback) {
-    let options = {
+    const options = {
       method: 'POST',
       url: 'https://prod2.click-v.com/ds_platform/brand/getBrandDetailOutSide',
       headers: {
         'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
         'Content-Type': 'application/json'
       },
-      body: {"brandId": task.id, "pageSize": 10, "pageNum": 1},
+      body: { brandId: task.id, pageSize: 10, pageNum: 1 },
       json: true
     };
     req(options, (error, response, body) => {
       if (error) {
         logger.debug('视频总量请求失败', error.message);
-        return callback(error)
+        callback(error);
+        return;
       }
-      if (response.statusCode != 200) {
+      if (response.statusCode !== 200) {
         logger.debug('视频总量状态码', response.statusCode);
-        return callback(response.statusCode)
+        callback(response.statusCode);
+        return;
       }
-      let user = {
+      const user = {
         bid: task.id,
         platform: task.p,
         fans_num: body.data.attentionNum
       };
       task.total = body.data.count;
-      //this.sendUser(user);
+      // this.sendUser(user);
       this.sendStagingUser(user);
       this.getList(task, options, () => {
-        callback()
-      })
-    })
+        callback();
+      });
+    });
   }
   sendUser(user) {
-    let option = {
+    const option = {
       url: this.settings.sendFans,
       data: user
     };
@@ -67,119 +69,115 @@ class dealWith {
       if (err) {
         logger.error('occur error : ', err);
         logger.info(`返回点视用户 ${user.bid} 连接服务器失败`);
-        return
+        return;
       }
       try {
-        back = JSON.parse(back.body)
+        back = JSON.parse(back.body);
       } catch (e) {
         logger.error(`点视用户 ${user.bid} json数据解析失败`);
         logger.info(back);
-        return
+        return;
       }
-      if (back.errno == 0) {
-        logger.debug("点视用户:", user.bid + ' back_end');
+      if (Number(back.errno) === 0) {
+        logger.debug('点视用户:', `${user.bid} back_end`);
       } else {
-        logger.error("点视用户:", user.bid + ' back_error');
+        logger.error('点视用户:', `${user.bid} back_error`);
         logger.info(back);
-        logger.info(`user info: `, user)
+        logger.info('user info: ', user);
       }
-    })
+    });
   }
   sendStagingUser(user) {
-    let option = {
+    const option = {
       url: 'http://staging-dev.meimiaoip.com/index.php/Spider/Fans/postFans',
       data: user
     };
     request.post(logger, option, (err, result) => {
       if (err) {
         logger.error('occur error : ', err);
-        return
+        return;
       }
       try {
-        result = JSON.parse(result.body)
+        result = JSON.parse(result.body);
       } catch (e) {
         logger.error('json数据解析失败');
         logger.info('send error:', result);
-        return
+        return;
       }
-      if (result.errno == 0) {
-        logger.debug("用户:", user.bid + ' back_end')
+      if (Number(result.errno) === 0) {
+        logger.debug('用户:', `${user.bid} back_end`);
       } else {
-        logger.error("用户:", user.bid + ' back_error');
-        logger.info(result)
+        logger.error('用户:', `${user.bid} back_error`);
+        logger.info(result);
       }
-    })
+    });
   }
   getList(task, options, callback) {
-    let page = 1,
-      total = task.total % 10 === 0 ? task.total / 10 : Math.ceil(task.total / 10);
+    let page = 1;
+    const total = task.total % 10 === 0 ? task.total / 10 : Math.ceil(task.total / 10);
     async.whilst(
-      () => {
-        return page < Math.min(total, 500)
-      },
+      () => page < Math.min(total, 500),
       (cb) => {
         options.body.pageNum = page;
         req(options, (error, response, body) => {
           if (error) {
             logger.debug('视频列表请求失败', error.message);
-            return callback(error)
+            callback(error);
+            return;
           }
-          if (response.statusCode != 200) {
+          if (response.statusCode !== 200) {
             logger.debug('视频列表状态码', response.statusCode);
-            return callback(response.statusCode)
+            callback(response.statusCode);
+            return;
           }
           this.deal(task, body.data.list, () => {
-            page++;
-            cb()
-          })
-        })
+            page += 1;
+            cb();
+          });
+        });
       },
-      (err, result) => {
-        callback()
+      () => {
+        callback();
       }
-    )
+    );
   }
   deal(task, info, callback) {
-    let index = 0,
-      length = info.length;
+    let index = 0;
+    const length = info.length;
     async.whilst(
-      () => {
-        return index < length
-      },
+      () => index < length,
       (cb) => {
         this.getInfo(task, info[index], () => {
-          index++;
-          cb()
-        })
+          index += 1;
+          cb();
+        });
       },
-      (err, data) => {
-        callback()
+      () => {
+        callback();
       }
-    )
+    );
   }
   getInfo(task, data, callback) {
-    let aid = data.id;
-       /*
-        *   没有发布时间
-        * */
-        let media = {
-          bid: task.id,
-          author: task.name,
-          platform: task.p,
-          aid: data.videoId,
-          title: spiderUtils.stringHandling(data.videoTitle, 80),
-          play_num: data.playTimes,
-          comment_num: data.commentCount,
-          save_num: data.approveTimes,
-          forward_num: data.shareTimes,
-          v_img: data.videoCoverUrl,
-          class: data.classifyName,
-          v_url: 'http://www.click-v.com/v3/html/videoshare.html?userId=0&videoId=' + data.videoId,
-          desc: spiderUtils.stringHandling(data.videoDescription, 100),
-          long_t: data.videoLength
-        };
-        spiderUtils.saveCache(this.core.cache_db, 'cache', media);
-        callback();
+    // const aid = data.id;
+    const media = {
+      bid: task.id,
+      author: task.name,
+      platform: task.p,
+      aid: data.videoId,
+      title: spiderUtils.stringHandling(data.videoTitle, 80),
+      play_num: data.playTimes,
+      comment_num: data.commentCount,
+      save_num: data.approveTimes,
+      forward_num: data.shareTimes,
+      v_img: data.videoCoverUrl,
+      class: data.classifyName,
+      // v_url: `http://www.click-v.com/v3/html/videoshare.html?userId=0&videoId=${data.videoId}`,
+      desc: spiderUtils.stringHandling(data.videoDescription, 100),
+      long_t: data.videoLength
+    };
+    // 没有发布时间
+    spiderUtils.saveCache(this.core.cache_db, 'cache', media);
+    callback();
   }
 }
-module.exports = dealWith
+module.exports = dealWith;
