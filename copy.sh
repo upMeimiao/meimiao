c26aaf33ca1323c0304a66858d0db2efa73d031b
junhao <changjunhao@outlook.com>
2017-04-27 18:10:45 +0800

    copy shell add copy commStart

--- copy.sh
+++ copy.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-path='/root/newStart/'
+path1='/root/newStart/'
+path2='/root/commStart/'
 #这里的-d 参数判断$path是否存在
-if [ ! -d ${path} ]
+if [ ! -d ${path1} ]
 then
     mkdir /root/newStart/
 fi
 cp ~/qiaosuan/newStart/* ~/newStart/
+
+if [ ! -d ${path2} ]
+then
+    mkdir /root/commStart/
+fi
+cp ~/qiaosuan/commStart/* ~/commStart/
