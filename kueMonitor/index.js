6a511217af915323cb4627ac34fe185b661d37eb
junhao <changjunhao@outlook.com>
2017-04-28 16:58:44 +0800

    评论kue

--- kueMonitor/index.js
+++ kueMonitor/index.js
@@ -5,14 +5,21 @@ const cors = require('cors');
 const basicAuth = require('basic-auth-connect');
 
 const app = express();
-
-let logger;
 class kueMonitor {
   constructor(settings) {
     this.settings = settings;
     this.redis = settings.redis;
-    logger = this.settings.logger;
+    this.logger = this.settings.logger;
+  }
+  start() {
+    let prefix = 'q';
+    if (this.settings.type === 'comment') {
+      prefix = 'c';
+    } else if (this.settings.type === 'video') {
+      prefix = 'q';
+    }
     kue.createQueue({
+      prefix,
       redis: {
         port: this.redis.port,
         host: this.redis.host,
@@ -20,22 +27,21 @@ class kueMonitor {
         db: this.redis.jobDB
       }
     });
-  }
-  start() {
-        // ui.setup({
-        //     apiURL: '/api',
-        //     baseURL: '/kue',
-        //     updateInterval: 5000
-        // })
-    kueUiExpress(app, '/kue/', '/api');
     app.use(basicAuth('verona', '2319446'));
     app.use(cors());
-        // app.use('/api', kue.app)
-        // app.use('/kue', ui.app)
-    app.use('/api', kue.app);
-    app.use('/kue', kue.app);
-    app.listen(3000);
-    logger.debug('UI started on port 3000');
+    if (this.settings.type === 'comment') {
+      kueUiExpress(app, '/c/kue/', '/c/api');
+      app.use('/c/api', kue.app);
+      app.use('/c/kue', kue.app);
+      app.listen(3003);
+      this.logger.debug('UI started on port 3003');
+    } else if (this.settings.type === 'video') {
+      kueUiExpress(app, '/kue/', '/api');
+      app.use('/api', kue.app);
+      app.use('/kue', kue.app);
+      app.listen(3000);
+      this.logger.debug('UI started on port 3000');
+    }
   }
 }
 
