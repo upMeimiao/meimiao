06b991ee926b5d05db647cce4b7be48eff889e39
junhao <changjunhao@outlook.com>
2017-04-28 20:49:26 +0800

    debug summaries

--- monitor/public/javascripts/action.js
+++ monitor/public/javascripts/action.js
@@ -102,6 +102,34 @@ const vm = new Vue({
   filters: {
   },
   methods: {
+    getSummaries(param) {
+      const { columns, data } = param;
+      const sums = [];
+      columns.forEach((column, index) => {
+        if (index === 0) {
+          sums[index] = '合计';
+          return;
+        }
+        if (index !== 3) {
+          sums[index] = 'N/A';
+          return;
+        }
+        const values = data.map(item => Number(item[column.property]));
+        if (!values.every(value => isNaN(value))) {
+          sums[index] = values.reduce((prev, curr) => {
+            const value = Number(curr);
+            if (!isNaN(value)) {
+              return prev + curr;
+            } else {
+              return prev;
+            }
+          }, 0);
+        } else {
+          sums[index] = 'N/A';
+        }
+      });
+      return sums;
+    },
     changePlatform(lab) {
       this.refresh(lab);
     },
