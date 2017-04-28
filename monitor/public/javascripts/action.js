d42c41560b64882dee6d417ee2db38e5eb8a63ff
junhao <changjunhao@outlook.com>
2017-04-28 21:46:25 +0800

    heartbeat add commentNum

--- monitor/public/javascripts/action.js
+++ monitor/public/javascripts/action.js
@@ -263,7 +263,7 @@ const vm = new Vue({
     socket.on('cache', (data) => {
       this.$notify.info({
         title: '暂存队列消息',
-        message: `目前缓存队列中有${data.num}条数据`,
+        message: `目前视频队列中有${data.videoNum}条数据，评论队列中有${data.commentNum}条数据`,
         duration: 360000
       });
     });
