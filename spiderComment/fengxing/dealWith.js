ccef24ef8d572bb3ce3e3cd90eaa43a160d531b4
zhugazi <zhu_gazi@163.com>
2017-05-04 19:43:40 +0800

    'fengxing'

--- spiderComment/fengxing/dealWith.js
+++ spiderComment/fengxing/dealWith.js
@@ -4,7 +4,7 @@
 const request = require('../../lib/request');
 const async = require('async');
 const Utils = require('../../lib/spiderUtils');
-const md5 = require('js-md5');
+const md5 = require('crypto').createHash('md5');
 
 let logger;
 class dealWith {
@@ -22,119 +22,125 @@ class dealWith {
     task.addCount = 0;      // 新增的评论数
     this.total(task, (err, result) => {
       if (result == 'add_0') {
-        return callback(null);
+        callback(null);
+        return;
       }
       callback(null, task.cNum, task.lastId, task.lastTime, task.addCount);
     });
   }
   total(task, callback) {
-    let option = {
-        url: `http://api1.fun.tv/comment/display/gallery/${task.bid}?pg=1&isajax=1&dtime=${new Date().getTime()}`
-      },
-      total = 0;
+    const option = {
+      url: `http://api1.fun.tv/comment/display/gallery/${task.bid}?pg=1&isajax=1&dtime=${new Date().getTime()}`
+    };
+    let total = 0;
     request.get(logger, option, (err, result) => {
       if (err) {
         logger.debug('风行网的评论总数请求失败');
-        return this.total(task, callback);
+        callback(err);
+        return;
       }
       try {
         result = JSON.parse(result.body);
       } catch (e) {
         logger.debug('风行网数据解析失败');
         logger.info(result);
-        return this.total(task, callback);
+        callback(e);
+        return;
       }
       task.cNum = result.data.total_num;
-      if ((task.cNum - task.commentNum) <= 0) {
-        return callback(null, 'add_0');
+      if ((task.cNum - task.commentNum) <= 0 || result.data.comment.length <= 0) {
+        callback(null, 'add_0');
+        return;
       }
       if (task.commentNum <= 0) {
-        total = (task.cNum % 20) == 0 ? task.cNum / 20 : Math.ceil(task.cNum / 20);
+        total = (task.cNum % 20) === 0 ? task.cNum / 20 : Math.ceil(task.cNum / 20);
       } else {
         total = (task.cNum - task.commentNum);
-        total = (total % 20) == 0 ? total / 20 : Math.ceil(total / 20);
+        total = (total % 20) === 0 ? total / 20 : Math.ceil(total / 20);
       }
-      let comment = result.data.comment[0],
-        cid = md5(task.bid + comment.user_id + comment.time);
+      const comment = result.data.comment[0],
+        cid = md5.update(task.bid + comment.user_id + comment.time).digest('hex');
       task.lastTime = comment.time;
       task.lastId = cid;
       task.addCount = task.cNum - task.commentNum;
-      this.commentList(task, total, (err) => {
+      this.commentList(task, total, () => {
         callback();
       });
     });
   }
   commentList(task, total, callback) {
-    let page = 1,
-      option;
+    let page = 1;
+    const option = {};
     async.whilst(
-			() => page <= total,
-			(cb) => {
-  option = {
-    url: `http://api1.fun.tv/comment/display/gallery/${task.bid}?pg=${page}&isajax=1&dtime=${new Date().getTime()}`
-  };
-  request.get(logger, option, (err, result) => {
-    if (err) {
-      logger.debug('风行网评论列表请求失败', err);
-      return cb();
-    }
-    try {
-      result = JSON.parse(result.body);
-    } catch (e) {
-      logger.debug('风行网评论数据解析失败');
-      logger.info(result);
-      return cb();
-    }
-    this.deal(task, result.data.comment, (err) => {
-      if (task.isEnd) {
-        return callback();
+      () => page <= total,
+      (cb) => {
+        option.url = `http://api1.fun.tv/comment/display/gallery/${task.bid}?pg=${page}&isajax=1&dtime=${new Date().getTime()}`;
+        request.get(logger, option, (err, result) => {
+          if (err) {
+            logger.debug('风行网评论列表请求失败', err);
+            cb();
+            return;
+          }
+          try {
+            result = JSON.parse(result.body);
+          } catch (e) {
+            logger.debug('风行网评论数据解析失败');
+            logger.info(result);
+            cb();
+            return;
+          }
+          this.deal(task, result.data.comment, () => {
+            if (task.isEnd) {
+              total = -1;
+              cb();
+              return;
+            }
+            page += 1;
+            cb();
+          });
+        });
+      },
+      () => {
+        callback();
       }
-      page++;
-      cb();
-    });
-  });
-},
-			(err, result) => {
-  callback();
-}
-		);
+    );
   }
   deal(task, comments, callback) {
-    let length = comments.length,
-      index = 0,
+    const length = comments.length;
+    let index = 0,
       cid,
       comment;
     async.whilst(
-			() => index < length,
-			(cb) => {
-  cid = md5(task.bid + comments[index].user_id + comments[index].time);
-  if (task.commentId == cid || task.commentTime >= comments[index].time) {
-    task.isEnd = true;
-    return callback();
-  }
-  comment = {
-    cid,
-    content: Utils.stringHandling(comments[index].content),
-    platform: task.p,
-    bid: task.bid,
-    aid: task.aid,
-    ctime: comments[index].time,
-    support: comments[index].upCount,
-    c_user: {
-      uid: comments[index].user_id,
-      uname: comments[index].nick_name,
-      uavatar: comments[index].user_icon.orig
-    }
-  };
-  Utils.commentCache(this.core.cache_db, comment);
-				// Utils.saveCache(this.core.cache_db,'comment_cache',comment)
-  index++;
-  cb();
-},
-			(err, result) => {
-  callback();
-}
-		);
+      () => index < length,
+      (cb) => {
+        cid = md5.update(task.bid + comments[index].user_id + comments[index].time).digest('hex');
+        if (task.commentId == cid || task.commentTime >= comments[index].time) {
+          task.isEnd = true;
+          callback();
+          return;
+        }
+        comment = {
+          cid,
+          content: Utils.stringHandling(comments[index].content),
+          platform: task.p,
+          bid: task.bid,
+          aid: task.aid,
+          ctime: comments[index].time,
+          support: comments[index].upCount,
+          c_user: {
+            uid: comments[index].user_id,
+            uname: comments[index].nick_name,
+            uavatar: comments[index].user_icon.orig
+          }
+        };
+        Utils.commentCache(this.core.cache_db, comment);
+        index += 1;
+        cb();
+      },
+      () => {
+        callback();
+      }
+    );
   }
 
 }
