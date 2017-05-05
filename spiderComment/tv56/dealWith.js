e6975a929e5908108a2dbca223eeb8a609bf6362
junhao <changjunhao@outlook.com>
2017-05-05 17:32:48 +0800

    eslint tv56

--- spiderComment/tv56/dealWith.js
+++ spiderComment/tv56/dealWith.js
@@ -1,9 +1,9 @@
 /**
 * Created by junhao on 2017/2/10.
 */
-const request = require('../../lib/request');
 const async = require('async');
-const Utils = require('../../lib/spiderUtils');
+const request = require('../../lib/request');
+const spiderUtils = require('../../lib/spiderUtils');
 
 let logger;
 class dealWith {
@@ -20,120 +20,128 @@ class dealWith {
     task.isEnd = false;  // 判断当前评论跟库里返回的评论是否一致
     task.addCount = 0;      // 新增的评论数
     this.totalPage(task, (err, result) => {
-      if (result == 'add_0') {
-        return callback(null);
+      if (err) {
+        callback(err);
+        return;
+      }
+      if (result === 'add_0') {
+        callback(null);
+        return;
       }
       callback(null, task.cNum, task.lastId, task.lastTime, task.addCount);
     });
   }
   totalPage(task, callback) {
-    let option = {
-        url: `${this.settings.tv56.total}${task.aid}&topic_url=http://my.tv.sohu.com/us/${task.bid}/${task.aid}.shtml&_=${new Date().getTime()}`
-      },
-      total = 0;
+    const option = {
+      url: `${this.settings.tv56.total}${task.aid}&topic_url=http://my.tv.sohu.com/us/${task.bid}/${task.aid}.shtml&_=${new Date().getTime()}`
+    };
+    let total = 0;
     request.get(logger, option, (err, result) => {
       if (err) {
         logger.debug('tv56评论总量请求失败', err);
-        return this.totalPage(task, callback);
+        callback(err);
+        return;
       }
       try {
         result = JSON.parse(result.body);
       } catch (e) {
         logger.debug('tv56评论数据解析失败');
         logger.info(result.body);
-        return this.totalPage(task, callback);
+        callback(err);
+        return;
       }
       task.cNum = result.cmt_sum;
       if ((task.cNum - task.commentNum) <= 0) {
-        return callback(null, 'add_0');
+        callback(null, 'add_0');
+        return;
       }
       if (task.commentNum <= 0) {
-        total = (task.cNum % 10) == 0 ? task.cNum / 10 : Math.ceil(task.cNum / 10);
+        total = (task.cNum % 10) === 0 ? task.cNum / 10 : Math.ceil(task.cNum / 10);
       } else {
         total = (task.cNum - task.commentNum);
-        total = (total % 10) == 0 ? total / 10 : Math.ceil(total / 10);
+        total = (total % 10) === 0 ? total / 10 : Math.ceil(total / 10);
       }
       task.topic_id = result.topic_id;
       task.lastTime = result.comments[0].create_time / 1000;
       task.lastId = result.comments[0].comment_id;
       task.addCount = task.cNum - task.commentNum;
-      this.commentList(task, total, (err) => {
+      this.commentList(task, total, () => {
         callback();
       });
     });
   }
   commentList(task, total, callback) {
-    let page = 1,
-      option;
+    let page = 1;
+    const option = {};
     async.whilst(
-			() => page <= total,
-			(cb) => {
-  option = {
-    url: `${this.settings.tv56.list}${task.topic_id}&page_no=${page}&_${new Date().getTime()}`
-  };
-  request.get(logger, option, (err, result) => {
-    if (err) {
-      logger.debug('tv56评论列表请求失败', err);
-      return cb();
-    }
-    try {
-      result = JSON.parse(result.body);
-    } catch (e) {
-      logger.debug('tv56评论数据解析失败');
-      logger.info(result);
-      return cb();
-    }
-    this.deal(task, result.comments, (err) => {
-      if (task.isEnd) {
-        return callback();
+      () => page <= total,
+      (cb) => {
+        option.url = `${this.settings.tv56.list}${task.topic_id}&page_no=${page}&_${new Date().getTime()}`;
+        request.get(logger, option, (err, result) => {
+          if (err) {
+            logger.debug('tv56评论列表请求失败', err);
+            cb();
+            return;
+          }
+          try {
+            result = JSON.parse(result.body);
+          } catch (e) {
+            logger.debug('tv56评论数据解析失败');
+            logger.info(result);
+            cb();
+            return;
+          }
+          this.deal(task, result.comments, () => {
+            if (task.isEnd) {
+              callback();
+              return;
+            }
+            page += 1;
+            cb();
+          });
+        });
+      },
+      () => {
+        callback();
       }
-      page++;
-      cb();
-    });
-  });
-},
-			(err, result) => {
-  callback();
-}
-		);
+    );
   }
   deal(task, comments, callback) {
-    let length = comments.length,
-      index = 0,
+    const length = comments.length;
+    let index = 0,
       comment;
     async.whilst(
-			() => index < length,
-			(cb) => {
-  if (task.commentId == comments[index].comment_id || task.commentTime >= comments[index].create_time / 1000) {
-    task.isEnd = true;
-    return callback();
+      () => index < length,
+      (cb) => {
+        if (task.commentId == comments[index].comment_id || task.commentTime >= comments[index].create_time / 1000) {
+          task.isEnd = true;
+          callback();
+          return;
+        }
+        comment = {
+          cid: comments[index].comment_id,
+          content: spiderUtils.stringHandling(comments[index].content),
+          platform: task.p,
+          bid: task.bid,
+          aid: task.aid,
+          ctime: comments[index].create_time / 1000,
+          support: comments[index].support_count,
+          step: comments[index].oppose_count,
+          c_user: {
+            uid: comments[index].passport.user_id,
+            uname: comments[index].passport.nickname,
+            uavatar: comments[index].passport.img_url
+          }
+        };
+        spiderUtils.saveCache(this.core.cache_db, 'comment_cache', comment);
+        index += 1;
+        cb();
+      },
+      () => {
+        callback();
+      }
+    );
   }
-  comment = {
-    cid: comments[index].comment_id,
-    content: Utils.stringHandling(comments[index].content),
-    platform: task.p,
-    bid: task.bid,
-    aid: task.aid,
-    ctime: comments[index].create_time / 1000,
-    support: comments[index].support_count,
-    step: comments[index].oppose_count,
-    c_user: {
-      uid: comments[index].passport.user_id,
-      uname: comments[index].passport.nickname,
-      uavatar: comments[index].passport.img_url
-    }
-  };
-  Utils.commentCache(this.core.cache_db, comment);
-				// Utils.saveCache(this.core.cache_db,'comment_cache',comment)
-  index++;
-  cb();
-},
-			(err, result) => {
-  callback();
-}
-		);
-  }
-
 }
 
 module.exports = dealWith;
