7476c7fdcb6c5dcf299acdc086422947e492f54b
zhugazi <zhu_gazi@163.com>
2017-05-05 11:43:41 +0800

    comment-pptv

--- spiderComment/pptv/hostTime.js
+++ spiderComment/pptv/hostTime.js
@@ -18,12 +18,12 @@ class hostTime {
     async.parallel(
       {
         hot: (cb) => {
-          this.getHot(task, (err) => {
+          this.getHot(task, () => {
             cb(null, '热门评论数据完成');
           });
         },
         time: (cb) => {
-          this.getTime(task, (err) => {
+          this.getTime(task, () => {
             cb(null, '最新评论数据完成');
           });
         }
@@ -35,8 +35,8 @@ class hostTime {
         );
   }
   getHot(task, callback) {
-    let page = 0,
-      total = Number(this.settings.commentTotal) % 20 == 0 ? Number(this.settings.commentTotal) / 20 : Math.ceil(Number(this.settings.commentTotal) / 20),
+    let page = 0;
+    const total = Number(this.settings.commentTotal) % 20 == 0 ? Number(this.settings.commentTotal) / 20 : Math.ceil(Number(this.settings.commentTotal) / 20),
       option = {};
     async.whilst(
             () => page < total,
@@ -46,97 +46,100 @@ class hostTime {
               request.get(logger, option, (err, result) => {
                 if (err) {
                   logger.debug('pptv评论列表请求失败', err);
-                  return cb();
+                  cb();
+                  return;
                 }
                 try {
                   result = JSON.parse(result.body);
                 } catch (e) {
                   logger.debug('pptv评论数据解析失败');
                   logger.info(result);
-                  return cb();
+                  cb();
+                  return;
                 }
                 if (result.data.page_list.length <= 0) {
                   page += total;
-                  return cb();
+                  cb();
+                  return;
                 }
-                this.deal(task, result.data.page_list, (err) => {
-                  page++;
+                this.deal(task, result.data.page_list, () => {
+                  page += 1;
                   callback();
                 });
               });
             },
-            (err, result) => {
+            () => {
               callback();
             }
         );
   }
   getTime(task, callback) {
-    let page = 0,
-      total = Number(this.settings.commentTotal) % 20 == 0 ? Number(this.settings.commentTotal) / 20 : Math.ceil(Number(this.settings.commentTotal) / 20),
-      option = {},
-      offset = 0;
+    let page = 0;
+    const total = Number(this.settings.commentTotal) % 20 == 0 ? Number(this.settings.commentTotal) / 20 : Math.ceil(Number(this.settings.commentTotal) / 20),
+      option = {};
     async.whilst(
-            () => page <= total,
-            (cb) => {
-              option = {
-                url: `http://apicdn.sc.pptv.com/sc/v3/pplive/ref/vod_${task.aid}/feed/list?appplt=web&action=1&pn=${page}&ps=20&from=web&version=1.0.0`
-              };
-              request.get(logger, option, (err, result) => {
-                if (err) {
-                  logger.debug('pptv评论列表请求失败', err);
-                  return cb();
-                }
-                try {
-                  result = JSON.parse(result.body);
-                } catch (e) {
-                  logger.debug('pptv评论数据解析失败');
-                  logger.info(result);
-                  return cb();
-                }
-                if (result.data.page_list.length <= 0) {
-                  page += total;
-                  return cb();
-                }
-                this.deal(task, result.data.page_list, (err) => {
-                  page++;
-                  cb();
-                });
-              });
-            },
-            (err, result) => {
-              callback();
-            }
-        );
+      () => page <= total,
+      (cb) => {
+        option.url = `http://apicdn.sc.pptv.com/sc/v3/pplive/ref/vod_${task.aid}/feed/list?appplt=web&action=1&pn=${page}&ps=20&from=web&version=1.0.0`;
+        request.get(logger, option, (err, result) => {
+          if (err) {
+            logger.debug('pptv评论列表请求失败', err);
+            cb();
+            return;
+          }
+          try {
+            result = JSON.parse(result.body);
+          } catch (e) {
+            logger.debug('pptv评论数据解析失败');
+            logger.info(result);
+            cb();
+            return;
+          }
+          if (result.data.page_list.length <= 0) {
+            page += total;
+            cb();
+            return;
+          }
+          this.deal(task, result.data.page_list, () => {
+            page += 1;
+            cb();
+          });
+        });
+      },
+      () => {
+        callback();
+      }
+    );
   }
   deal(task, comments, callback) {
-    let length = comments.length,
-      index = 0,
+    const length = comments.length;
+    let index = 0,
       comment;
     async.whilst(
-            () => index < length,
-            (cb) => {
-              comment = {
-                cid: comments[index].id,
-                content: Utils.stringHandling(comments[index].content),
-                platform: task.p,
-                bid: task.bid,
-                aid: task.aid,
-                ctime: comments[index].create_time / 1000,
-                support: comments[index].up_ct,
-                reply: comments[index].reply_ct,
-                c_user: {
-                  uname: comments[index].user.nick_name,
-                  uavatar: comments[index].user.icon
-                }
-              };
-              Utils.saveCache(this.core.cache_db, 'comment_update_cache', comment);
-              index++;
-              cb();
-            },
-            (err, result) => {
-              callback();
-            }
-        );
+      () => index < length,
+      (cb) => {
+        comment = {
+          cid: comments[index].id,
+          content: Utils.stringHandling(comments[index].content),
+          platform: task.p,
+          bid: task.bid,
+          aid: task.aid,
+          ctime: comments[index].create_time / 1000,
+          support: comments[index].up_ct,
+          reply: comments[index].reply_ct,
+          c_user: {
+            uname: comments[index].user.nick_name,
+            uavatar: comments[index].user.icon
+          }
+        };
+        Utils.saveCache(this.core.cache_db, 'comment_update_cache', comment);
+        index += 1;
+        cb();
+      },
+      () => {
+        callback();
+      }
+    );
   }
 }
 module.exports = hostTime;
