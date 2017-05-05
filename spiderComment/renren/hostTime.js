60859daf6a686aefa896506fba21ee1c8ed17cc7
zhugazi <zhu_gazi@163.com>
2017-05-05 12:00:33 +0800

    comment-renren

--- spiderComment/renren/hostTime.js
+++ spiderComment/renren/hostTime.js
@@ -21,8 +21,8 @@ class hostTime {
   }
   commentInfo(task, callback) {
     let page = 1,
-      cycle = true,
-      total = this.core.settings.commentTotal % 10 == 0 ? this.core.settings.commentTotal / 10 : Math.ceil(this.core.settings.commentTotal / 10),
+      cycle = true;
+    const total = this.core.settings.commentTotal % 10 === 0 ? this.core.settings.commentTotal / 10 : Math.ceil(this.core.settings.commentTotal / 10),
       option = {
         url: 'http://web.rr.tv/v3plus/comment/list',
         headers: {
@@ -36,71 +36,75 @@ class hostTime {
         }
       };
     async.whilst(
-            () => cycle,
-            (cb) => {
-              option.data.page = page;
-              request.post(logger, option, (err, result) => {
-                if (err) {
-                  logger.debug(option);
-                  logger.debug('人人视频评论列表接口请求失败', err);
-                  return cb();
-                }
-                try {
-                  result = JSON.parse(result.body);
-                } catch (e) {
-                  logger.debug('解析失败', result.body);
-                  return cb();
-                }
-                if (result.data.results <= 0) {
-                  cycle = false;
-                  return cb();
-                }
-                if (page >= total) {
-                  cycle = false;
-                  return cb();
-                }
-                this.deal(task, result.data.results, (err) => {
-                  page++;
-                  cb();
-                });
-              });
-            },
-            (err, result) => {
-              callback();
-            }
-        );
+      () => cycle,
+      (cb) => {
+        option.data.page = page;
+        request.post(logger, option, (err, result) => {
+          if (err) {
+            logger.debug(option);
+            logger.debug('人人视频评论列表接口请求失败', err);
+            cb();
+            return;
+          }
+          try {
+            result = JSON.parse(result.body);
+          } catch (e) {
+            logger.debug('解析失败', result.body);
+            cb();
+            return;
+          }
+          if (result.data.results <= 0) {
+            cycle = false;
+            cb();
+            return;
+          }
+          if (page >= total) {
+            cycle = false;
+            cb();
+            return;
+          }
+          this.deal(task, result.data.results, () => {
+            page += 1;
+            cb();
+          });
+        });
+      },
+      () => {
+        callback();
+      }
+    );
   }
   deal(task, comments, callback) {
-    let length = comments.length,
-      index = 0,
+    const length = comments.length;
+    let index = 0,
       cid,
       comment;
     async.whilst(
-            () => index < length,
-            (cb) => {
-              cid = comments[index].id;
-              comment = {
-                cid,
-                content: Utils.stringHandling(comments[index].content),
-                platform: task.p,
-                bid: task.bid,
-                aid: task.aid,
-                ctime: comments[index].createTime,
-                support: comments[index].likeCount,
-                c_user: {
-                  uid: comments[index].author.id,
-                  uname: comments[index].author.nickName,
-                  uavatar: comments[index].author.headImgUrl
-                }
-              };
-              Utils.saveCache(this.core.cache_db, 'comment_update_cache', comment);
-              index++;
-              cb();
-            },
-            (err, result) => {
-              callback();
-            }
-        );
+      () => index < length,
+      (cb) => {
+        cid = comments[index].id;
+        comment = {
+          cid,
+          content: Utils.stringHandling(comments[index].content),
+          platform: task.p,
+          bid: task.bid,
+          aid: task.aid,
+          ctime: comments[index].createTime,
+          support: comments[index].likeCount,
+          c_user: {
+            uid: comments[index].author.id,
+            uname: comments[index].author.nickName,
+            uavatar: comments[index].author.headImgUrl
+          }
+        };
+        Utils.saveCache(this.core.cache_db, 'comment_update_cache', comment);
+        index += 1;
+        cb();
+      },
+      () => {
+        callback();
+      }
+   );
   }
 }
 module.exports = hostTime;
