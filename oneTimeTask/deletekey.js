d5d257a3675934f45cfc24c9035c82d7dcce4c4d
junhao <changjunhao@outlook.com>
2017-05-10 16:39:27 +0800

    deletekey

--- oneTimeTask/deletekey.js
+++ oneTimeTask/deletekey.js
@@ -1,7 +1,7 @@
 const Redis = require('ioredis');
 const async = require('async');
 
-const redis = new Redis('redis://:C19prsPjHs52CHoA0vm@r-m5e43f2043319e64.redis.rds.aliyuncs.com:6379/2', {
+const redis = new Redis('redis://:C19prsPjHs52CHoA0vm@r-m5e43f2043319e64.redis.rds.aliyuncs.com:6379/3', {
   reconnectOnError(err) {
     return err.message.slice(0, 'READONLY'.length) === 'READONLY';
   }
@@ -25,9 +25,20 @@ function loop(info) {
     () => {
       console.log('end');
       redis.quit();
+      // redis.scan('0', 'MATCH', 'c:*', 'COUNT', '2000', (err, result) => {
+      //   if (result.length === 0) {
+      //     console.log('end');
+      //     redis.quit();
+      //   } else {
+      //     loop(result[1]);
+      //   }
+      // });
     }
   );
 }
 redis.keys('c:*', (err, result) => {
   loop(result);
 });
+// redis.scan('0', 'MATCH', 'c:*', 'COUNT', '2000', (err, result) => {
+//   loop(result[1]);
+// });
