375a4801891cf12e6cab0512c6fe24a98adf46b7
junhao <changjunhao@outlook.com>
2017-05-22 15:00:44 +0800

    debug yotube

--- test/youtube/dealWith.js
+++ test/youtube/dealWith.js
@@ -271,7 +271,7 @@ class dealWith {
         v: video.encrypted_id
       },
       headers: {
-        'accept-language': 'rzh-CN,zh;q=0.8',
+        'accept-language': 'zh-CN,zh;q=0.8',
         referer: `https://m.youtube.com/watch?v=${video.encrypted_id}`,
         'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
       }
