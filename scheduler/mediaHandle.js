c18e6e87ff1ae52606cae730cf233ba8a7b68c96
junhao <changjunhao@outlook.com>
2017-05-27 13:10:10 +0800

    open tudou

--- scheduler/mediaHandle.js
+++ scheduler/mediaHandle.js
@@ -35,10 +35,10 @@ class mediaHandle {
     //   encodeId: _.encodeId ? _.encodeId : '',
     //   type: _.type ? _.type : ''
     // }
-    if (Number(_.platform) === 12) {
-      callback();
-      return;
-    }
+    // if (Number(_.platform) === 12) {
+    //   callback();
+    //   return;
+    // }
     const platform = platformMap.get(Number(_.platform)),
       baseInfo = {
         id: _.bid,
