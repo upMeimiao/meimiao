3b3f4393c51ec833943c433de798eeb7c769768e
junhao <changjunhao@outlook.com>
2017-06-27 19:06:55 +0800

    debug facebook

--- spider/facebook/dealWith.js
+++ spider/facebook/dealWith.js
@@ -339,8 +339,8 @@ class dealWith {
         };
         media = spiderUtils.deleteProperty(media);
         spiderUtils.saveCache(this.core.cache_db, 'cache', media);
-        // spiderUtils.commentSnapshots(this.core.taskDB,
-        //   { p: media.platform, aid: media.aid, comment_num: media.comment_num });
+        spiderUtils.commentSnapshots(this.core.taskDB,
+          { p: media.platform, aid: media.aid, comment_num: media.comment_num });
         callback();
       }
     );
