4816e65db608d478ef946c755f985038e7701556
junhao <changjunhao@outlook.com>
2017-07-06 10:52:17 +0800

    优化clean脚本

--- clean.sh
+++ clean.sh
@@ -2,27 +2,27 @@
 echo "开启清理巧算日志文件"
 
 cd /root/qiaosuan/instance/master/logs
-rm -rf *.log
+#rm -rf *.log
 rm -rf *.log-*
 rm -rf *.log.*.gz
 cd /root/qiaosuan/instance/scheduler/logs
-rm -rf *.log
+#rm -rf *.log
 rm -rf *.log-*
 rm -rf *.log.*.gz
 cd /root/qiaosuan/instance/monitorSpider/logs
-rm -rf *.log
+#rm -rf *.log
 rm -rf *.log-*
 rm -rf *.log.*.gz
 cd /root/qiaosuan/instance/overseas/logs
-rm -rf *.log
+#rm -rf *.log
 rm -rf *.log-*
 rm -rf *.log.*.gz
 cd /root/qiaosuan/instance/comment/logs
-rm -rf *.log
+#rm -rf *.log
 rm -rf *.log-*
 rm -rf *.log.*.gz
 cd /root/qiaosuan/instance/comment_overseas/logs
-rm -rf *.log
+#rm -rf *.log
 rm -rf *.log-*
 rm -rf *.log.*.gz
 
@@ -33,5 +33,4 @@ echo "开启pm2日志文件"
 pm2 flush
 cd /root/.pm2/logs
 rm -rf ./*__*.log
-rm -rf ./*
 echo "pm2日志文件清理完毕"
