2144cf1161624cd5de776bcf2c47985325f19579
junhao <changjunhao@outlook.com>
2017-07-13 20:19:21 +0800

    monitor add youku api

--- monitor/controllers/youku.js
+++ monitor/controllers/youku.js
@@ -4,7 +4,7 @@
 const Redis = require('ioredis');
 const async = require('neo-async');
 
-const redis = new Redis('redis://:C19prsPjHs52CHoA0vm@r-m5e970ad613f13a4.redis.rds.aliyuncs.com:6379/1', {
+const redis = new Redis('redis://:C19prsPjHs52CHoA0vm@r-m5e970ad613f13a4.redis.rds.aliyuncs.com:6379/15', {
   reconnectOnError(err) {
     return err.message.slice(0, 'READONLY'.length) === 'READONLY';
   }
