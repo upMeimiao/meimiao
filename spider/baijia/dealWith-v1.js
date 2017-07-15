cdc2aea9d0a2d899009ad0d4e873f1d00f0ffe8d
zhugazi <zhu_gazi@163.com>
2017-07-15 15:33:56 +0800

    debug-baijia

--- spider/baijia/dealWith-v1.js
+++ spider/baijia/dealWith-v1.js
@@ -7,16 +7,6 @@ const cheerio = require('cheerio');
 const request = require('../../lib/request');
 const spiderUtils = require('../../lib/spiderUtils');
 
-const _tag = (tags) => {
-  let tag = '';
-  if (!tags || !tags.length) {
-    return '';
-  }
-  for (const value of tags) {
-    tag += `,${value.name}`;
-  }
-  return tag.replace(',', '');
-};
 let logger;
 class dealWith {
   constructor(spiderCore) {
@@ -28,61 +18,46 @@ class dealWith {
   todo(task, callback) {
     task.total = 0;
     task.isEnd = false;
-    async.parallel(
-      {
-        user: (cb) => {
-          this.getUser(task, () => {
-            cb(null, '粉丝数已返回');
-          });
-        },
-        media: (cb) => {
-          this.getVidList(task, () => {
-            cb(null, '视频信息已返回');
-          });
-        }
-      },
-      (err, result) => {
-        logger.debug('result: ', result);
-        callback(null, task.total);
-      }
-    );
-  }
-  getUser(task, callback) {
-    const option = {
-      url: this.settings.spiderAPI.baijia.api,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Mobile/14F89 haokan/2.6.1 (Baidu; P2 10.3.2)/2.3.01_2,8enohP/381d/C2BB16A6BC640F0CD9DA2060098AC66793B62A080FCTOPTMEQM/1',
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      data: {
-        'baijia/authorInfo': `method=get&app_id=${task.id}`
-      }
-    };
-    request.post(logger, option, (err, result) => {
+    this.getVidList(task, (err) => {
       if (err) {
-        logger.debug('用户粉丝数请求失败', err);
         callback(err);
         return;
       }
-      try {
-        result = JSON.parse(result.body);
-      } catch (e) {
-        logger.error('百家号用户数据解析失败', result.body);
-        callback(e);
+      callback(null, task.total);
+    });
+  }
+  getFan(task, vid) {
+    const option = {
+      url: `https://baijiahao.baidu.com/po/feed/video?wfr=spider&for=pc&context=%7B%22sourceFrom%22%3A%22bjh%22%2C%22nid%22%3A%22${vid}%22%7D`
+    };
+    request.get(logger, option, (err, result) => {
+      if (err) {
+        logger.debug('用户粉丝数请求失败');
+        this.getFan(task, vid);
         return;
       }
-      if (!result['baijia/authorInfo']) {
-        callback('user-fans-error');
+      const $ = cheerio.load(result.body);
+      if ($('div.item p').eq(0).text() === '视频已失效，请观看其他视频') {
+        return;
+      }
+      result = result.body.replace(/[\s\n\r]/g, '');
+      const startIndex = result.indexOf('videoData={"id'),
+        endIndex = result.indexOf(';window.listInitData');
+      let dataJson = result.substring(startIndex + 10, endIndex);
+      try {
+        dataJson = JSON.parse(dataJson);
+      } catch (e) {
+        logger.error('百家号用户数据解析失败');
         return;
       }
       const user = {
         bid: task.id,
         platform: task.p,
-        fans_num: result['baijia/authorInfo'].data.subscribe_total
+        fans_num: dataJson.app.fans_cnt
       };
-      // this.sendUser(user);
+      task.isEnd = true;
+      this.sendUser(user);
       this.sendStagingUser(user);
-      callback();
     });
   }
   sendUser(user) {
@@ -139,58 +114,56 @@ class dealWith {
   }
   getVidList(task, callback) {
     const option = {
-      url: `${this.settings.spiderAPI.baijia.videoList + task.id}&_limit=10000&_skip=0`,
       referer: `http://baijiahao.baidu.com/u?app_id=${task.id}&fr=bjhvideo`,
       ua: 1
     };
-    let videoList = [];
-    request.get(logger, option, (err, result) => {
-      if (err) {
-        logger.error('baijia视频列表请求错误 : ', err);
-        callback(err);
-        return;
-      }
-      try {
-        result = JSON.parse(result.body);
-      } catch (e) {
-        logger.error('baijia-json数据总量解析失败', result.body);
-        callback(e);
-        return;
-      }
-      if (!result.items) {
-        callback('baijia-list-error');
-        return;
-      }
-      for (const value of result.items) {
-        if (value.type === 'video') {
-          videoList.push({
-            aid: value.id,
-            title: value.title,
-            desc: value.abstract,
-            tag: value.tag || '',
-            v_img: JSON.parse(value.cover_images)[0].src,
-            v_url: `http://baijiahao.baidu.com/builder/preview/s?id=${value.tp_id}`,
-            comment_num: value.comment_amount,
-            forward_num: value.push_amount,
-            a_create_time: moment(new Date(value.publish_at)).format('X'),
-            feed_id: value.feed_id,
-            tpId: value.tp_id
+    let skip = 0, cycle = true;
+    async.whilst(
+      () => cycle,
+      (cb) => {
+        option.url = `${this.settings.spiderAPI.baijia.videoList + task.id}&_limit=50&_skip=${skip}`;
+        request.get(logger, option, (err, result) => {
+          if (err) {
+            logger.error('视频列表请求错误 : ', err);
+            cb();
+            return;
+          }
+          try {
+            result = JSON.parse(result.body);
+          } catch (e) {
+            logger.error('json数据总量解析失败');
+            logger.info(result);
+            cb();
+            return;
+          }
+          if (!result.items) {
+            cycle = false;
+            cb();
+            return;
+          }
+          this.deal(task, result.items, () => {
+            skip += 50;
+            if (skip > 10000) {
+              cycle = false;
+              cb();
+              return;
+            }
+            cb();
           });
-        }
-      }
-      this.deal(task, videoList, () => {
+        });
+      },
+      () => {
         callback();
-      });
-      videoList = null;
-    });
+      }
+    );
   }
-  deal(task, list, callback) {
+  deal(task, user, callback) {
     let index = 0;
-    const length = list.length;
+    const length = user.length;
     async.whilst(
       () => index < length,
       (cb) => {
-        this.getAllInfo(task, list[index], () => {
+        this.getAllInfo(task, user[index], () => {
           index += 1;
           cb();
         });
@@ -201,55 +174,67 @@ class dealWith {
     );
   }
   getAllInfo(task, video, callback) {
+    if (video.type !== 'video') {
+      callback();
+      return;
+    }
     async.parallel([
       (cb) => {
-        this.getVideoInfo(video.v_url, (err, result) => {
-          cb(null, result);
-        });
+        if (video.feed_id === '') {
+          this.getVideoInfo(null, video.url, (err, result) => {
+            cb(null, result);
+          });
+        } else {
+          if (!task.isEnd) {
+            this.getFan(task, video.feed_id);
+          }
+          this.getVideoInfo(video.feed_id, null, (err, result) => {
+            cb(null, result);
+          });
+        }
       }
     ], (err, result) => {
-      const media = {
-        author: task.name,
-        platform: task.p,
-        bid: task.id,
-        aid: video.aid,
-        title: spiderUtils.stringHandling(video.title, 100),
-        desc: spiderUtils.stringHandling(video.desc, 100),
-        class: result[0].class || '',
-        tag: result[0].tag,
-        long_t: result[0].long_t || '',
-        v_img: video.v_img,
-        v_url: video.v_url,
-        comment_num: video.comment_num,
-        forward_num: video.forward_num,
-        a_create_time: video.a_create_time,
-        play_num: result[0].playNum
-      };
+      const time = new Date(video.publish_at),
+        media = {
+          author: task.name,
+          platform: task.p,
+          bid: task.id,
+          aid: video.id,
+          title: spiderUtils.stringHandling(video.title, 100),
+          desc: spiderUtils.stringHandling(video.abstract, 100),
+          class: video.domain,
+          tag: video.tag,
+          long_t: result[0].long_t,
+          v_img: JSON.parse(video.cover_images)[0].src,
+          v_url: video.url,
+          comment_num: video.comment_amount,
+          forward_num: video.push_amount,
+          a_create_time: moment(time).format('X'),
+          play_num: result[0].playNum
+        };
       if (!media.play_num && media.play_num !== 0) {
         callback();
         return;
       }
-      if (Number(media.long_t) === -1) {
-        callback();
-        return;
-      }
       task.total += 1;
-      // logger.debug(media);
+      logger.debug(media);
       spiderUtils.saveCache(this.core.cache_db, 'cache', media);
       spiderUtils.commentSnapshots(this.core.taskDB,
         { p: media.platform, aid: media.aid, comment_num: media.comment_num });
       callback();
     });
   }
-  getVideoInfo(url, callback) {
-    const option = {
-      url,
-      ua: 1
-    };
+  getVideoInfo(vid, url, callback) {
+    const option = {};
+    if (vid !== null) {
+      option.url = `https://baijiahao.baidu.com/po/feed/video?wfr=spider&for=pc&context=%7B%22sourceFrom%22%3A%22bjh%22%2C%22nid%22%3A%22${vid}%22%7D`;
+    } else {
+      option.url = url;
+    }
     request.get(logger, option, (err, result) => {
       if (err) {
         logger.debug('单个视频请求失败 ', err);
-        callback(null, { long_t: '', playNum: '' });
+        callback(null, { long_t: '', a_create_time: '', playNum: '' });
         return;
       }
       const $ = cheerio.load(result.body);
@@ -267,15 +252,13 @@ class dealWith {
       } catch (e) {
         logger.debug('百家号用户数据解析失败');
         logger.info(dataJson);
-        callback(null, { long_t: '', playNum: '' });
+        callback(null, { long_t: '', a_create_time: '', playNum: '' });
         return;
       }
       const time = dataJson.video ? dataJson.video.time_length : `json${dataJson.article.content}`;
       const res = {
         long_t: this.getVidTime(time),
-        playNum: dataJson.video ? dataJson.video.playcnt : dataJson.article.read_amount,
-        class: dataJson.article ? dataJson.article.domain : '',
-        tag: _tag(dataJson.article.tag)
+        playNum: dataJson.video ? dataJson.video.playcnt : dataJson.article.read_amount
       };
       callback(null, res);
     });
