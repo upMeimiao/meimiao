63371b34712090e1e39dc0cb14fbf5ae0e161b06
junhao <changjunhao@outlook.com>
2017-07-25 18:16:09 +0800

    debug facebook

--- spider/facebook/index.js
+++ spider/facebook/index.js
@@ -16,7 +16,7 @@ class spiderCore {
     this.settings = settings;
     this.redis = settings.redis;
     this.dealWith = new (require('./dealWith'))(this);
-    this.cookies = '';
+    this.cookies = 'locale=zh_CN; datr=uarsWNHwHCDMME4QegGkXoHN; sb=6YrtWNWdRZc0ZD6Icppm_tQu; pl=n; lu=gA; c_user=100017290094462; xs=37%3AxIXj8PMRj_BcyQ%3A2%3A1500607556%3A-1%3A-1; fr=0r8fZNLbaDU2imrlv.AWW1_eZHu3l7c40wbgZm9zQ_Jmw.BZK-yc.9Q.Flx.0.0.BZdw5Q.AWU90BI5; act=1500974734029%2F4; presence=EDvF3EtimeF1500977306EuserFA21B17290094462A2EstateFDutF1500977306434CEchFDp_5f1B17290094462F1CC';
     logger = settings.logger;
     logger.trace('spiderCore instantiation ...');
   }
@@ -56,7 +56,7 @@ class spiderCore {
         return;
       }
       this.auth = auth;
-      this.getCookie(auth, () => {
+      // this.getCookie(auth, () => {
         // process.env.NODE_ENV = 'production';
         if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
           this.deal();
@@ -64,7 +64,7 @@ class spiderCore {
           this.test();
         }
       });
-    });
+    // });
   }
   getCookie(auth, callback) {
     const parameter = {
