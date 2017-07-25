1323b0910ac503b75482e772dd792b3cd88c9b1f
zhugazi <zhu_gazi@163.com>
2017-07-25 19:42:12 +0800

    酷六加上评论数

--- spider/ku6/dealWith.js
+++ spider/ku6/dealWith.js
@@ -78,9 +78,7 @@ class dealWith {
           bid: task.id,
           fans_num: spiderUtils.numberHandling(fans)
         };
-      this.sendUser(user, (err, result) => {
-        callback();
-      });
+      this.sendUser(user, () => callback());
       this.sendStagingUser(user);
     });
   }
@@ -91,16 +89,18 @@ class dealWith {
     };
     request.post(logger, option, (err, result) => {
       if (err) {
-        return callback(err);
+        callback(err);
+        return;
       }
       try {
         result = JSON.parse(result.body);
       } catch (e) {
         logger.error('json数据解析失败');
         logger.info('send error:', result);
-        return callback(e);
+        callback(e);
+        return;
       }
-      if (result.errno == 0) {
+      if (result.errno === 0) {
         logger.debug('酷6用户:', `${user.bid} back_end`);
       } else {
         logger.error('酷6用户:', `${user.bid} back_error`);
@@ -125,7 +125,7 @@ class dealWith {
         logger.info('send error:', result);
         return;
       }
-      if (result.errno == 0) {
+      if (result.errno === 0) {
         logger.debug('用户:', `${user.bid} back_end`);
       } else {
         logger.error('用户:', `${user.bid} back_error`);
@@ -142,20 +142,23 @@ class dealWith {
     };
     request.get(logger, option, (err, result) => {
       if (err) {
-        return callback(err);
+        callback(err);
+        return;
       }
       try {
         result = JSON.parse(result.body);
       } catch (e) {
         logger.error('json数据解析失败');
         logger.info('json1 error :', result.body);
-        return callback(e);
+        callback(e);
+        return;
       }
       const total = result.data.videoCount;
       task.total = total;
-      this.getList(task, total, (err) => {
-        if (err) {
-          return callback(err);
+      this.getList(task, total, (error) => {
+        if (error) {
+          callback(error);
+          return;
         }
         callback(null, '视频信息已返回');
       });
@@ -167,88 +170,126 @@ class dealWith {
       page,
       option,
       list;
-    if (total % 20 == 0) {
+    if (total % 20 === 0) {
       page = total / 20;
     } else {
       page = Math.ceil(total / 20);
     }
     async.whilst(
-            () => sign <= Math.min(page, 500),
-            (cb) => {
-              logger.debug(`开始获取第${sign}页视频列表`);
-              option = {
-                url: `${this.settings.spiderAPI.ku6.allInfo + task.id}&pn=${newSign}`
-              };
-              request.get(logger, option, (err, result) => {
-                if (err) {
-                  return cb();
-                }
-                try {
-                  result = JSON.parse(result.body);
-                } catch (e) {
-                  logger.error('json数据解析失败');
-                  logger.info('json error: ', result.body);
-                  sign++;
-                  newSign++;
-                  return cb();
-                }
-                list = result.data;
-                if (list) {
-                  this.deal(task, list, () => {
-                    sign++;
-                    newSign++;
-                    cb();
-                  });
-                } else {
-                  sign++;
-                  newSign++;
-                  cb();
-                }
-              });
-            },
-            (err, result) => {
-              callback();
-            }
-        );
+      () => sign <= Math.min(page, 500),
+      (cb) => {
+        logger.debug(`开始获取第${sign}页视频列表`);
+        option = {
+          url: `${this.settings.spiderAPI.ku6.allInfo + task.id}&pn=${newSign}`
+        };
+        request.get(logger, option, (err, result) => {
+          if (err) {
+            cb();
+            return;
+          }
+          try {
+            result = JSON.parse(result.body);
+          } catch (e) {
+            logger.error('json数据解析失败');
+            logger.info('json error: ', result.body);
+            sign += 1;
+            newSign += 1;
+            cb();
+            return;
+          }
+          list = result.data;
+          if (list) {
+            this.deal(task, list, () => {
+              sign += 1;
+              newSign += 1;
+              cb();
+            });
+          } else {
+            sign += 1;
+            newSign += 1;
+            cb();
+          }
+        });
+      },
+      () => callback()
+    );
   }
   deal(task, list, callback) {
     let index = 0;
     async.whilst(
-            () => index < list.length,
-            (cb) => {
-              this.getInfo(task, list[index], (err) => {
-                index++;
-                cb();
-              });
-            },
-            (err, result) => {
-              callback();
-            }
-        );
+      () => index < list.length,
+      (cb) => {
+        this.getInfo(task, list[index], () => {
+          index += 1;
+          cb();
+        });
+      },
+      () => callback()
+    );
   }
   getInfo(task, data, callback) {
-    let time = data.uploadtime,
-      a_create_time = time.substring(0, 10),
-      media = {
-        author: data.nick,
-        platform: 14,
-        bid: task.id,
-        aid: data.vid,
-        title: data.title ? data.title.substr(0, 100).replace(/"/g, '') : 'btwk_caihongip',
-        desc: data.desc.substr(0, 100).replace(/"/g, ''),
-        play_num: data.viewed,
-        support: data.liked,
-        step: data.disliked,
-        a_create_time,
-        long_t: data.videotime,
-        v_img: this._v_img(data.picpath),
-        tag: this._tag(data.tag),
-        class: this._class(data.catename)
-      };
-    spiderUtils.saveCache(this.core.cache_db, 'cache', media);
-    spiderUtils.commentSnapshots(this.core.taskDB,
-      { p: media.platform, aid: media.aid, comment_num: media.comment_num });
-    callback();
+    async.waterfall(
+      [
+        (cb) => {
+          this.getComment(data, (err, num) => {
+            cb(null, num);
+          });
+        }
+      ],
+      (err, result) => {
+        const time = data.uploadtime,
+          a_create_time = time.substring(0, 10),
+          media = {
+            author: data.nick,
+            platform: 14,
+            bid: task.id,
+            aid: data.vid,
+            title: data.title ? data.title.substr(0, 100).replace(/"/g, '') : 'btwk_caihongip',
+            desc: data.desc.substr(0, 100).replace(/"/g, ''),
+            play_num: data.viewed,
+            support: data.liked,
+            step: data.disliked,
+            a_create_time,
+            long_t: data.videotime,
+            v_img: this._v_img(data.picpath),
+            tag: this._tag(data.tag),
+            class: this._class(data.catename),
+            comment_num: result
+          };
+        spiderUtils.saveCache(this.core.cache_db, 'cache', media);
+        // logger.info(media);
+        spiderUtils.commentSnapshots(this.core.taskDB,
+          { p: media.platform, aid: media.aid, comment_num: media.comment_num });
+        callback();
+      }
+    );
+  }
+  getComment(video, callback) {
+    const option = {
+      url: `${this.settings.spiderAPI.ku6.comment + video.vid}&pn=1`
+    };
+    request.get(logger, option, (err, result) => {
+      if (err) {
+        logger.error('ku6评论总量请求失败', err);
+        return;
+      }
+      try {
+        result = JSON.parse(result.body);
+      } catch (e) {
+        logger.error('ku6评论数据解析失败', result.body);
+        callback(null, '');
+        return;
+      }
+      if (Number(result.status) === 300) {
+        callback(null, '');
+        return;
+      }
+      if (!result.data) {
+        callback(null, '');
+        return;
+      }
+      callback(null, result.data.count);
+    });
   }
   _tag(raw) {
     if (!raw) {
